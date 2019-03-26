3e9f49a847948359293071e343d45d371f2762ac
steffeli <steffen.lien@ehelse.no>
2019-03-26 10:45:21 +0100

    chore: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const pkg = require('./package.json');
+
+const DtsBundlerWebpackPlugin = require('dtsbundler-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, pkg.source),
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: pkg.main,
+    libraryTarget: 'commonjs2'
+  },
+  plugins: [
+      new DtsBundlerWebpackPlugin({
+          out: 'index.d.ts'
+      })
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.(ts|tsx)$/,
+            use: [
+                {
+                    loader: 'babel-loader'
+                },
+                {
+                    loader: 'ts-loader'
+                }
+            ]
+        }
+    ]
+  },
+  resolve: {
+      alias: {
+          'react': path.resolve(__dirname, 'node_modules/react'),
+          'react-dom': path.resolve(__dirname, 'node_modules/react-dom')
+      },
+      extensions: ['.ts', '.tsx', '.js']
+  },
+  externals: {
+    react: {
+        commonjs: 'react',
+        commonjs2: 'react',
+        amd: 'React',
+        root: 'React'
+    },
+    'react-dom': {
+        commonjs: 'react-dom',
+        commonjs2: 'react-dom',
+        amd: 'ReactDOM',
+        root: 'ReactDOM'
+    }
+  }
+};
