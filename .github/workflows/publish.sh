c343f38922def140157d8ec9018c75ed92f050eb
Steffen Lien <steffen.lien@ehelse.no>
2019-08-21 10:48:00 +0200

    dev: remove dry-run from publish

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -1,7 +1,7 @@
 # Have to run this in order to filter on tags due to: https://github.com/actions/bin/issues/66
 if git describe --tags | grep -E "^v0.0.1-dev.[0-9]{1,}[[:cntrl:]]*$";
 then
-  npm publish --tag dev --dry-run
+  npm publish --tag dev
 else
   echo "Not tagged, skipping release"
 fi
