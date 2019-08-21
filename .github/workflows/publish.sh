0fb6ce28874e6b168c2514d4a9a7c4d640edc4dc
Steffen Lien <steffen.lien@ehelse.no>
2019-08-21 10:36:53 +0200

    dev: add an comment to explain the tag filtering issue and reinstate GHP publish

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -1,3 +1,4 @@
+# Have to run this in order to filter on tags due to: https://github.com/actions/bin/issues/66
 if git describe --tags | grep -E "^v0.0.1-dev.[0-9]{1,}[[:cntrl:]]*$";
 then
   npm publish --tag dev --dry-run
