75edd826edaed3c98e9dc6a7224a7dd0f7f22d31
Steffen Lien <steffen.lien@ehelse.no>
2019-08-26 09:53:42 +0200

    dev: add a fork of bootstrap grid

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import postcss from 'rollup-plugin-postcss';
 import buble from 'rollup-plugin-buble';
 import progress from 'rollup-plugin-progress';
-import dts from 'rollup-plugin-dts';
 import sizes from 'rollup-plugin-sizes';
-import sass from 'node-sass';
-import autoprefixer from 'autoprefixer';
 import copy from 'rollup-plugin-copy';
 import pkg from './package.json';
+
+import sass from 'node-sass';
+import postcss from 'rollup-plugin-postcss';
+import autoprefixer from 'autoprefixer';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
@@ -45,11 +45,11 @@ export default {
       exclude: 'node_modules/**',
     }),
     postcss({
-      preprocessor: (content, id) =>
-        new Promise((resolve, reject) => {
-          const result = sass.renderSync({file: id});
-          resolve({code: result.css.toString()});
-        }),
+      // preprocessor: (content, id) =>
+      //   new Promise((resolve, reject) => {
+      //     const result = sass.renderSync({file: id});
+      //     resolve({code: result.css.toString()});
+      //   }),
       minimize: true,
       plugins: [autoprefixer],
       sourceMap: true,
