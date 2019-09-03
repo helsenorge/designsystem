9836e212f184d529ee500f5ecaa35ac41f75c816
Steffen Lien <steffen.lien@ehelse.no>
2019-09-03 15:19:43 +0200

    dev: remove some code to prep for another test release

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -1,5 +1,4 @@
 # Have to run this in order to filter on tags due to: https://github.com/actions/bin/issues/66
-# Adding a test comment to trigger github actions
 if git describe --tags | grep -E "^v0.0.1-dev.[0-9]{1,}[[:cntrl:]]*$";
 then
   npm publish --tag dev
