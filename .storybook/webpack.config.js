c729dab55dc6997bfe119f1bf36e0b133b15f9b0
Steffen Lien <steffen.lien@ehelse.no>
2019-10-21 15:36:25 +0200

    dev: update stencil packages

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,9 +4,9 @@ const CopyPlugin = require('copy-webpack-plugin');
 const WriteFilePlugin = require('write-file-webpack-plugin');
 
 module.exports = async ({config}) => {
-  config.entry.push(path.join(__dirname, '../dist/hnds.js'));
-  fs.readdirSync(path.join(__dirname, '../dist/collection/components')).map(function(file) {
-    jsFilePath = path.join(__dirname, `../dist/collection/components/${file}/${file}.js`);
+  config.entry.push(path.join(__dirname, '../dist/helsenorge.js'));
+  fs.readdirSync(path.join(__dirname, '../dist/collection/')).map(function(file) {
+    jsFilePath = path.join(__dirname, `../dist/collection/${file}/${file}.js`);
     try {
       if (fs.existsSync(jsFilePath)) {
         config.entry.push(jsFilePath);
@@ -15,7 +15,7 @@ module.exports = async ({config}) => {
       console.error(err);
     }
 
-    cssFilePath = path.join(__dirname, `../dist/collection/components/${file}/${file}.css`);
+    cssFilePath = path.join(__dirname, `../dist/collection/${file}/${file}.css`);
     try {
       if (fs.existsSync(cssFilePath)) {
         config.entry.push(cssFilePath);
--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
-  const presets = [
-    '@babel/preset-env',
-    '@babel/preset-typescript',
-    '@babel/preset-react',
-  ];
-  const plugins = [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread'
-  ];
+  const presets = ['@babel/preset-env', '@babel/preset-typescript'];
+  const plugins = ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-object-rest-spread'];
   return {
     presets,
     plugins,
