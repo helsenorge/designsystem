9a6478cbc3a450c1e1ac944c0ceef41e9f6126f1
Steffen Lien <steffen.lien@ehelse.no>
2019-10-21 16:31:15 +0200

    dev: merge in develop

--- src/components/StartButton/StartButton.tsx
+++ src/components/StartButton/StartButton.tsx
@@ -3,20 +3,28 @@ import cn from 'classnames';
 
 import './StartButton.scss';
 
-export type StartButtonVariants = 'primary' | 'secondary' | 'tertiary';
+// TODO: Make this inherit from a set of base variant types in constants.
+export type StartButtonVariant = string | 'seconary' | 'tertiary' | undefined | null;
 
 interface StartButtonProps
   extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-  children?: React.ReactNode;
-  variant?: StartButtonVariants;
+  children: React.ReactNode;
+  variant?: StartButtonVariant;
+  isLoading?: boolean;
 }
 
+// TODO: Move most of the logic out in a generic Button-component that the others inherit.
 const StartButton = React.forwardRef((props: StartButtonProps, ref: any) => {
-  const {children, variant = 'primary', ...rest} = props;
-  const classes = cn('start-button', `start-button--${variant}`);
+  const {children, variant, isLoading = false, ...rest} = props;
+  const classes = cn(
+    'button is-start',
+    {[`is-${variant}`]: variant === 'secondary' || variant === 'tertiary'},
+    {['is-loading']: isLoading},
+  );
   return (
+    // TODO: Add a 'as' prop so that the button can be either an 'a'-tag or 'button'-tag.
     <button className={classes} ref={ref} {...rest}>
-      {children}
+      <span>{children}</span>
     </button>
   );
 });
--- stories/Buttons/StartButton.stories.tsx
+++ stories/Buttons/StartButton.stories.tsx
@@ -1,16 +1,60 @@
 import React from 'react';
 
-import {select, text, withKnobs} from '@storybook/addon-knobs';
+import {text, select, boolean, withKnobs} from '@storybook/addon-knobs';
 import {storiesOf} from '@storybook/react';
+
+import Stacker from '../../.storybook/Stacker/Stacker';
 
 const stories = storiesOf('Buttons', module);
 stories.addDecorator(withKnobs);
 
 import {StartButton} from '../../src/components/StartButton';
 
+const buttonVariants = {
+  primary: null,
+  secondary: 'secondary',
+  tertiary: 'tertiary'
+}
+
 stories.add('StartButton', (): JSX.Element => (
-  <StartButton
-    variant={select('variant', ['primary', 'secondary', 'tertiary'], 'primary')}>
-    {text('text', 'StartButton')}
-  </StartButton>
+    <StartButton
+      variant={select('variant', buttonVariants, buttonVariants.primary)}
+      isLoading={boolean('isLoading', false)}
+      disabled={boolean('disabled', false)}
+      >
+      {text('text', 'StartButton')}
+    </StartButton>
+));
+
+// stories.add('StartButton with icon', (): JSX.Element => (
+//     <StartButton
+//       iconLeft={<Icon size={32} color="white">lock</Icon>}
+//       variant={select('variant', ['primary', 'secondary', 'tertiary'], 'primary')}
+//       isLoading={boolean('isLoading', false)}
+//       disabled={boolean('disabled', false)}
+//       >
+//       {text('text', 'StartButton')}
+//     </StartButton>
+// ));
+
+stories.add('StartButton (all variants)', (): JSX.Element => (
+  <Stacker>
+    <StartButton
+      disabled={boolean('disabled', false)}
+      >
+      Primary
+    </StartButton>
+    <StartButton
+      variant="secondary"
+      disabled={boolean('disabled', false)}
+      >
+      Secondary
+    </StartButton>
+    <StartButton
+      variant="tertiary"
+      disabled={boolean('disabled', false)}
+      >
+      Tertiary
+    </StartButton>
+  </Stacker>
 ));
