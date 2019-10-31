97c2cf718ded805a0c3bae743b24a84188830565
Steffen Lien <steffen.lien@ehelse.no>
2019-10-31 11:16:19 +0100

    dev: adjust lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'src/**/*.+(js|jsx|json|yml|yaml|scss|ts|tsx)': ['yarn format', 'git add'],
+  'packages/**/*.+(js|jsx|json|yml|yaml|scss|ts|tsx)': ['yarn format', 'git add'],
 };
