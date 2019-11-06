f03ca7bda811c904272972ae72106a3bc9fb2207
Steffen Lien <steffen.lien@ehelse.no>
2019-11-06 14:48:50 +0000

    dev: add a slack notification for new packages (#77)

---
+++ .github/workflows/slack-notify.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+HOOK_URL=$1
+read -r -d '' MSG <<'EOF'
+payload={
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*New version has been published! :eyes:*\n All you have to do is force feed an upgrade command into your perfered terminal of choice."
+			},
+			"accessory": {
+				"type": "image",
+				"image_url": "https://media.giphy.com/media/Xhxd8T0og4oKs/giphy.gif",
+				"alt_text": "happy jerry"
+			}
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Upgrade with *Yarn*\n`yarn upgrade @helsenorge/designsystem@dev`"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Upgrade with *NPM*\n`npm update @helsenorge/designsystem@dev`"
+			}
+		}
+	]
+}
+EOF
+
+curl -X POST --data-urlencode "$MSG" $HOOK_URL
