213e0abeb89dd3f5d7338ebd1ac50fdd9e41a984
Steffen Lien <steffen.lien@ehelse.no>
2019-11-18 16:04:13 +0100

    ci: add new workflow for develop

--- .github/workflows/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Have to run this in order to filter on tags due to: https://github.com/actions/bin/issues/66
-if git describe --tags | grep -E "^v0.0.1-dev.[0-9]{1,}[[:cntrl:]]*$";
-then
-  npm publish --tag dev
-else
-  echo "Not tagged, skipping release"
-fi
