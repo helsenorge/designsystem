281787db3dbf6f95c3b5ebf236b2f845e353661b
Steffen Lien <steffen.lien@ehelse.no>
2019-11-25 14:34:52 +0100

    refactor(react): delete more dormant scss styling

--- packages/designsystem-react/src/components/Spinner/Spinner.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import {Spinner} from './Spinner';
-
-test('renders default variant correctly', (): void => {
-  const {container} = render(<Spinner />);
-  expect(container).toMatchSnapshot();
-});
