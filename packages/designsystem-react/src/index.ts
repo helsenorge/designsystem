16f3d7ea302577fab653f4d85f4eb5fecc448ebb
Steffen Lien <steffen.lien@ehelse.no>
2019-12-04 15:16:21 +0100

    fix: add test component

--- packages/designsystem-react/src/index.ts
+++ packages/designsystem-react/src/index.ts
@@ -4,5 +4,6 @@ export {StartButton} from './components/StartButton';
 export {ConfirmationButton} from './components/ConfirmationButton';
 export {CommonButton} from './components/CommonButton';
 export {Icon} from './components/Icons';
+export {TestComponent} from './components/TestComponent';
 // export {DiveBoard, DiveBoardTitle, DiveBoardPreamable} from './components/DiveBoard';
 export {Palette, IconSize} from './constants';
