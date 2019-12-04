7ef796e246ce440d9c20286303df6ba7598aa966
Steffen Lien <steffen.lien@ehelse.no>
2019-12-04 14:03:16 +0100

    fix: comment out diveboard

--- packages/designsystem-react/src/index.ts
+++ packages/designsystem-react/src/index.ts
@@ -3,5 +3,5 @@ export {StartButton} from './components/StartButton';
 export {ConfirmationButton} from './components/ConfirmationButton';
 export {CommonButton} from './components/CommonButton';
 export {Icon} from './components/Icons';
-export {DiveBoard, DiveBoardTitle, DiveBoardPreamable} from './components/DiveBoard';
+// export {DiveBoard, DiveBoardTitle, DiveBoardPreamable} from './components/DiveBoard';
 export {Palette, IconSize} from './constants';
