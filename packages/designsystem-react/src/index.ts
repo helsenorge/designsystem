c5f33b22b75766356493a2982998d814e3e6ffc4
Steffen Lien <steffen.lien@ehelse.no>
2019-12-04 15:41:20 +0100

    fix: rename this absurd component

--- packages/designsystem-react/src/index.ts
+++ packages/designsystem-react/src/index.ts
@@ -4,6 +4,6 @@ export {StartButton} from './components/StartButton';
 export {ConfirmationButton} from './components/ConfirmationButton';
 export {CommonButton} from './components/CommonButton';
 export {Icon} from './components/Icons';
-export {DiveBoard} from './components/DiveBoard';
+export {DiveBoardd} from './components/DiveBoardd';
 // export {DiveBoard, DiveBoardTitle, DiveBoardPreamable} from './components/DiveBoard';
 export {Palette, IconSize} from './constants';
