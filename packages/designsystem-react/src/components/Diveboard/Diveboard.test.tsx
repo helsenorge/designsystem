773fe286581c0e47793632051daa4e102be643e1
Steffen Lien <steffen.lien@ehelse.no>
2019-12-04 15:07:23 +0100

    fix: remove test completely

--- packages/designsystem-react/src/components/Diveboard/Diveboard.test.tsx
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import {render} from '@testing-library/react';
-// import {DiveBoard} from './DiveBoard';
-
-// test('renders default variant correctly', (): void => {
-//   const {container} = render(<DiveBoard></DiveBoard>);
-//   expect(container).toMatchSnapshot();
-// });
