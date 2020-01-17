e3d01c2c541fc92f7afd283ac4c2e852eddc1a49
Steffen Lien <steffen.lien@nhn.no>
2020-01-17 13:11:48 +0100

    feat(react): add list component

---
+++ packages/designsystem-react/src/components/List/List.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import List from './List';
+
+test('displays a list of 4 elements', (): void => {
+  const {container, getAllByRole} = render(
+    <List>
+      <h1>Tjeneste 1</h1>
+      <h1>Tjeneste 2</h1>
+      <h1>Tjeneste 3</h1>
+      <h1>Tjeneste 4</h1>
+    </List>,
+  );
+  expect(getAllByRole('heading').length).toEqual(4);
+  expect(container).toMatchSnapshot();
+});
