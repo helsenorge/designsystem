b894ecf52b17b6edce78cf31b16e9ede451c7e60
jamesbjerkholt <james@infinitesoup.com>
2020-01-28 11:39:52 +0100

    Docschanges (#97)
    * docs: replaced honest, demotivating text on frontpage with utopian goal
    
    * docs: add and change mdx-docs and menu order
    
    Added placeholder files for parts of theplanned content structure. Added content from 2 previously
    uncommitted component-page documents.
    
    * docs: add placeholderfiles for Brand-section of odcs, add prev content
    
    * style: added woff and woff2 files for all weights of Source Sans
    
    24 fontweights added for Source Sans Pro
    
    * style: update fontstack H1, H2, H3, Preamble +switch font to Source Sans
    
    * docs: add temporary Lighbox-page and switch image repo to nhn
    
    * docs: add introduction to designpattern section
    
    * docs: change url path and headline
    
    Co-authored-by: Steffen Lien <steffen.lien@mimedi.no>

--- packages/designsystem-docs/src/components/markdown/MDXCustom/Preamble.tsx
+++ packages/designsystem-docs/src/components/markdown/MDXCustom/Preamble.tsx
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
 const Preamble = styled('p')`
-  font-weight: 200;
-  font-size: 1.4rem;
+  font-weight: 600;
+  margin-top: 2.25rem;
+  margin-bottom: 2rem;
+  line-height: 2rem;
+  font-size: 1.125rem;
 `;
 
 export default Preamble;
