b3def7fae5c972ba50aaf0c6286d84ada5536445
Steffen Lien <steffen.lien@nhn.no>
2020-02-04 11:51:52 +0100

    build: remove bundled externals (#111)

--- packages/designsystem-react/rollup.config.js
+++ packages/designsystem-react/rollup.config.js
@@ -1,17 +1,13 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import commonjs from 'rollup-plugin-commonjs';
 import buble from 'rollup-plugin-buble';
 import progress from 'rollup-plugin-progress';
 import sizes from 'rollup-plugin-sizes';
 import execute from 'rollup-plugin-execute';
 
-import scss from 'rollup-plugin-scss';
-import copy from 'rollup-plugin-copy';
 import pkg from './package.json';
-
-// import postcss from 'rollup-plugin-postcss';
-// import autoprefixer from 'autoprefixer';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
@@ -32,15 +28,14 @@ export default {
     },
     {file: pkg.module, format: 'es', globals, sourcemap: true},
   ],
-  external: ['classnames', 'styled-components'],
   plugins: [
     progress({
       clearLine: false, // default: true
     }),
+    peerDepsExternal(),
     resolve({extensions}),
     commonjs({
       namedExports: {
-        cx: ['classNames'],
         '../../node_modules/react-is/index.js': ['isElement', 'isValidElementType', 'ForwardRef'],
       },
     }),
@@ -49,15 +44,6 @@ export default {
       include: ['src/**/*'],
       exclude: '/node_modules/',
     }),
-    // scss({
-    //   output: './dist/css/helsenorge.min.css',
-    //   outputStyle: 'compressed',
-    // }),
-    // copy({
-    //   targets: [{src: 'dist/*', dest: '../designsystem-react/dist'}],
-    //   copyOnce: true,
-    // }),
-    execute(["cd ./src/components/ ;find . -name '*.scss' | cpio -pdm  ../../dist/components/"]),
     buble(),
     sizes(),
   ],
