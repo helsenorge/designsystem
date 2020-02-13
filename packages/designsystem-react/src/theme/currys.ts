83843dc010143fc05209d13091faab36a801f245
Steffen Lien <steffen.lien@nhn.no>
2020-02-13 14:25:06 +0100

    feat: add white color

--- packages/designsystem-react/src/theme/currys.ts
+++ packages/designsystem-react/src/theme/currys.ts
@@ -4,4 +4,6 @@ import {theme} from './';
 const isNotPalette = (name: string) => name === 'white' || name === 'black';
 
 export const getColor = (name: PaletteNames | 'white' | 'black', depth: PaletteDepths) =>
-  isNotPalette(name) ? name : theme.palette[`${name}${depth}`];
+  isNotPalette(name) ? '#FFFFFF' : theme.palette[`${name}${depth}`];
+
+export const getHoverColor = (color: PaletteNames | 'white' | 'black') => `${getColor(color, 700)}1A`;
