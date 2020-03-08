ef43b5d4d7830810e2ea01287be857f70f902552
Steffen Lien <steffen.lien@nhn.no>
2020-03-08 12:58:03 +0100

    feat: tile component (#154)

---
+++ packages/designsystem-react/src/components/Tile/Tile.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import Tile from './Tile';
+import Icon from '../Icons';
+
+test('displays tile', (): void => {
+  const {container, getByText} = render(
+    <Tile icon={<Icon size={64} type="alarmClock" />} title="Title" description="Description" />,
+  );
+  expect(container).toMatchSnapshot();
+});
