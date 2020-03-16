f7d2f6d516f3d3c61f3fb1d9e60a26c891f5cd84
Steffen Lien <steffen.lien@nhn.no>
2020-03-16 10:13:32 +0100

    fix: add padding on oversized text for badge

--- packages/designsystem-react/src/components/Badge/Badge.tsx
+++ packages/designsystem-react/src/components/Badge/Badge.tsx
@@ -1,18 +1,37 @@
+import React from 'react';
 import styled, {css} from 'styled-components';
 import {PaletteNames} from '../../theme/palette';
 import {getColor} from '../../theme/currys/color';
 
 export type BadgeColors = PaletteNames;
+export type BadgeChildren = string | number;
 
 interface BadgeProps {
+  children: BadgeChildren;
   className?: string;
   color?: BadgeColors;
 }
 
-const Badge = styled('span')<BadgeProps>`
+const Badge = React.forwardRef((props: BadgeProps, ref: any) => {
+  const {children, className = '', color = 'black'} = props;
+  const oversized = children.toString().length > 2;
+  return (
+    <StyledBadge oversized={oversized} className={className} color={color} ref={ref}>
+      {children}
+    </StyledBadge>
+  );
+});
+
+interface StyledBadgeProps {
+  color: BadgeColors;
+  oversized: boolean;
+}
+
+const StyledBadge = styled('span')<StyledBadgeProps>`
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 0 ${props => (props.oversized ? '0.25rem' : 0)};
   font-size: 0.875rem;
   font-weight: 600;
   color: ${props => (props.color === 'white' ? 'black' : 'white')};
