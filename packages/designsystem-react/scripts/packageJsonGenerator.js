fe6f14a3e7db9a5a8c72d3a17b2806bd23c14ae4
Marion Le Pontois <marion.le.pontois@ehelse.no>
2020-05-27 11:48:44 +0200

    fix(build): wip

---
+++ packages/designsystem-react/scripts/packageJsonGenerator.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const fse = require('fs-extra');
+
+generatePackageJson();
+
+function generatePackageJson() {
+  return new Promise(resolve => {
+    fse.readFile(path.resolve(__dirname, '../package.json'), 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      resolve(data);
+    });
+  })
+    .then(data => JSON.parse(data))
+    .then(packageData => {
+      const {author, version, peerDependencies, license, dependencies} = packageData;
+
+      const minimalPackage = {
+        ...packageData,
+        scripts: {},
+      };
+      /*
+      const minimalPackage = {
+        name: '@helsenorge/designsystem-react',
+        author,
+        version,
+        scripts: { prepublishOnly: 'node .././node_modules/@helsenorge/core-build/lib/helsenorge-scripts.js prepublish' },
+        main: './index.js',
+        license,
+        peerDependencies,
+        dependencies,
+      };
+      */
+
+      return new Promise(resolve => {
+        const libPath = path.resolve(__dirname, '../dist/package.json');
+        const data = JSON.stringify(minimalPackage, null, 2);
+        fse.writeFile(libPath, data, err => {
+          if (err) {
+            console.log(err);
+            throw err;
+          }
+          console.log(`Created package.json in ${libPath}`);
+          resolve();
+        });
+      });
+    });
+}
