1d7d40f8557d2a0fe22383ec5af878a6dd443fc0
Marion Le Pontois <marion.le.pontois@ehelse.no>
2020-07-24 09:01:54 +0200

    fix(fix): fixing icon export

--- packages/designsystem-react/src/components/Icons/index.ts
+++ packages/designsystem-react/src/components/Icons/index.ts
@@ -1,4 +1,4 @@
-import {Icon, IconProps, IconRawProps, IconColors, SvgIcon} from './Icon';
+export * from './Icon';
 
-export {IconProps, IconRawProps, IconColors, SvgIcon};
+import {Icon} from './Icon';
 export default Icon;
