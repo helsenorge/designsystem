f3ff56bbf29cccbee3863abcf674f850b52f0166
Marion Le Pontois <marion.le.pontois@ehelse.no>
2020-07-29 12:16:04 +0200

    fix(types): fixing types for ExpanderList and LinkList

--- packages/designsystem-react/src/components/LinkList/LinkList.tsx
+++ packages/designsystem-react/src/components/LinkList/LinkList.tsx
@@ -11,7 +11,7 @@ import ChevronRight from '../Icons/ChevronRight';
 import {useHover} from '../../hooks/useHover';
 
 export type LinkListColors = PaletteNames;
-export type LinkType = React.ForwardRefExoticComponent<LinkProps & React.RefAttributes<unknown>>;
+export type LinkType = React.ForwardRefExoticComponent<LinkProps & React.RefAttributes<HTMLLIElement>>;
 export interface CompoundComponent
   extends React.ForwardRefExoticComponent<LinkListProps & React.RefAttributes<HTMLUListElement>> {
   Link: LinkType;
@@ -27,50 +27,45 @@ interface LinkListProps {
   large?: boolean;
 }
 
-interface LinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
+export interface LinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
   children: React.ReactNode;
   color?: LinkListColors;
   large?: boolean;
   chevron?: boolean;
   className?: string;
   icon?: React.ReactElement;
+  href?: string;
 }
 
-const Link: LinkType = React.forwardRef(
-  (
-    {children, className = '', color = 'neutral', icon, large = false, chevron = false, ...restProps}: LinkProps,
-    ref: any,
-  ) => {
-    const {hoverRef, isHovered} = useHover<HTMLAnchorElement>();
-    return (
-      <li ref={ref}>
-        <StyledLinkListLink
-          className={className}
-          ref={hoverRef}
-          hasIcon={!!(chevron || icon)}
-          large={large}
-          color={color}
-          {...restProps}>
-          <StyledLinkListLinkContent>
-            {icon && (
-              <StyledLinkListIconContainer>
-                {React.cloneElement(icon, {size: 48, isHovered})}
-              </StyledLinkListIconContainer>
-            )}
-            {children}
-          </StyledLinkListLinkContent>
-          {chevron && (
-            <StyledLinkListIconContainer>
-              <Icon svgIcon={ChevronRight} />
-            </StyledLinkListIconContainer>
+const Link: LinkType = React.forwardRef((props: LinkProps, ref: React.Ref<HTMLLIElement>) => {
+  const {children, className = '', color = 'neutral', icon, large = false, chevron = false, ...restProps} = props;
+  const {hoverRef, isHovered} = useHover<HTMLAnchorElement>();
+  return (
+    <li ref={ref}>
+      <StyledLinkListLink
+        ref={hoverRef}
+        className={className}
+        hasIcon={!!(chevron || icon)}
+        large={large}
+        color={color}
+        {...restProps}>
+        <StyledLinkListLinkContent>
+          {icon && (
+            <StyledLinkListIconContainer>{React.cloneElement(icon, {size: 48, isHovered})}</StyledLinkListIconContainer>
           )}
-        </StyledLinkListLink>
-      </li>
-    );
-  },
-);
+          {children}
+        </StyledLinkListLinkContent>
+        {chevron && (
+          <StyledLinkListIconContainer>
+            <Icon svgIcon={ChevronRight} />
+          </StyledLinkListIconContainer>
+        )}
+      </StyledLinkListLink>
+    </li>
+  );
+});
 
-const LinkList = React.forwardRef((props: LinkListProps, ref: any) => {
+const LinkList = React.forwardRef((props: LinkListProps, ref: React.Ref<HTMLUListElement>) => {
   const {children, className = '', chevron = false, large, color, topBorder = true, bottomBorder = true} = props;
   return (
     <StyledLinkList className={className} topBorder={topBorder} bottomBorder={bottomBorder} ref={ref}>
