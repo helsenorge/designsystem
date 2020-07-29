30fa2ba70350595d304a3c34ec0baa48deba445a
Marion Le Pontois <marion.le.pontois@ehelse.no>
2020-07-29 09:25:43 +0200

    fix(eslint): eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,12 @@ module.exports = {
     'prettier',
     'prettier/@typescript-eslint',
   ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 1,
+    'react/jsx-no-literals': 2,
+    'no-console': 1,
+    'no-debugger': 1,
+  },
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
