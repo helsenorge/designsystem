bb7621b177983c0a11d98ca6dd5b4779c02435ed
Marion Le Pontois <marion.le.pontois@ehelse.no>
2021-01-28 14:34:35 +0100

    legger til alias for å løse dobbel react issue

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -62,5 +62,11 @@ exports.onCreateWebpackConfig = ({ actions }) => {
     node: {
       fs: 'empty',
     },
+    resolve: {
+      alias: {
+        react: path.resolve('./node_modules/react'),
+        'react-dom': path.resolve('./node_modules/react-dom'),
+      },
+    },
   });
 };
