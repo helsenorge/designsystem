562b512f3798ef74766f28526098794c84798a7a
Marion Le Pontois <marion.le.pontois@ehelse.no>
2021-01-29 16:02:13 +0100

    endring til å trigge bygg

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     '@typescript-eslint/no-empty-interface': 1,
     '@typescript-eslint/no-explicit-any': 2,
     '@typescript-eslint/no-inferrable-types': 0,
-    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/camelcase': 2,
     'react/jsx-no-literals': 2,
     'react/no-string-refs': 1,
     'no-console': 2,
