9731eb63d71c0a5b7617c1a6a1448ee6e7a83e57
Marion Le Pontois <marion.le.pontois@ehelse.no>
2021-02-16 15:17:12 +0100

    adding support for css module typings

---
+++ npm/designsystem/src/components/NotificationPanel/styles.module.scss.d.ts
@@ -0,0 +1,76 @@
+export const notificationPanel: string
+export const notificationPanelSmall: string
+export const notificationPanelMedium: string
+export const notificationPanelLarge: string
+export const notificationPanelInfo: string
+export const notificationPanelWarn: string
+export const notificationPanelAlert: string
+export const notificationPanelCrisis: string
+export const notificationPanelShadow: string
+export const notificationPanelHaslabel: string
+export const notificationPanelFluidwrapper: string
+export const notificationPanelFluidwrapperInfo: string
+export const notificationPanelFluidwrapperWarn: string
+export const notificationPanelFluidwrapperAlert: string
+export const notificationPanelFluidwrapperCrisis: string
+export const notificationPanelFluidwrapperShadow: string
+export const notificationPanelIcon: string
+export const notificationPanelContent: string
+export const notificationPanelContentCrisis: string
+export const notificationPanelContentIsred: string
+export const notificationPanelContentHaslabel: string
+export const notificationPanelLabel: string
+export const notificationPanelActionColumn: string
+export const notificationPanelCloseButton: string
+interface Namespace {
+	"notificationPanel": string,
+	"notification-panel": string,
+	"notificationPanelSmall": string,
+	"notification-panel--small": string,
+	"notificationPanelMedium": string,
+	"notification-panel--medium": string,
+	"notificationPanelLarge": string,
+	"notification-panel--large": string,
+	"notificationPanelInfo": string,
+	"notification-panel--info": string,
+	"notificationPanelWarn": string,
+	"notification-panel--warn": string,
+	"notificationPanelAlert": string,
+	"notification-panel--alert": string,
+	"notificationPanelCrisis": string,
+	"notification-panel--crisis": string,
+	"notificationPanelShadow": string,
+	"notification-panel--shadow": string,
+	"notificationPanelHaslabel": string,
+	"notification-panel--haslabel": string,
+	"notificationPanelFluidwrapper": string,
+	"notification-panel__fluidwrapper": string,
+	"notificationPanelFluidwrapperInfo": string,
+	"notification-panel__fluidwrapper--info": string,
+	"notificationPanelFluidwrapperWarn": string,
+	"notification-panel__fluidwrapper--warn": string,
+	"notificationPanelFluidwrapperAlert": string,
+	"notification-panel__fluidwrapper--alert": string,
+	"notificationPanelFluidwrapperCrisis": string,
+	"notification-panel__fluidwrapper--crisis": string,
+	"notificationPanelFluidwrapperShadow": string,
+	"notification-panel__fluidwrapper--shadow": string,
+	"notificationPanelIcon": string,
+	"notification-panel__icon": string,
+	"notificationPanelContent": string,
+	"notification-panel__content": string,
+	"notificationPanelContentCrisis": string,
+	"notification-panel__content--crisis": string,
+	"notificationPanelContentIsred": string,
+	"notification-panel__content--isred": string,
+	"notificationPanelContentHaslabel": string,
+	"notification-panel__content--haslabel": string,
+	"notificationPanelLabel": string,
+	"notification-panel__label": string,
+	"notificationPanelActionColumn": string,
+	"notification-panel__action-column": string,
+	"notificationPanelCloseButton": string,
+	"notification-panel__close-button": string,
+}
+declare const stylesModuleScss: Namespace
+export default stylesModuleScss
