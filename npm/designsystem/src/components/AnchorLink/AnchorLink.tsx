0301e58df7223b368d7b54785563e6423104189f
Frederic Hermandsen <frederic.hermandsen@nhn.no>
2021-02-18 12:20:45 +0100

    endret styling til anchorlist

--- npm/designsystem/src/components/AnchorLink/AnchorLink.tsx
+++ npm/designsystem/src/components/AnchorLink/AnchorLink.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Icon from '../Icons';
 import ArrowUpRight from '../Icons/ArrowUpRight';
-import StyledAnchorLink from './AnchorLink.styled';
 import { getColor } from '../../theme/currys';
 import { useHover } from '../../hooks/useHover';
+
+import AnchorLinkStyles from './styles.module.scss';
+import classNames from 'classnames';
 
 export type AnchorLinkTargets = '_self' | '_blank' | '_parent';
 
@@ -19,19 +21,21 @@ const AnchorLink = React.forwardRef(function AnchorLinkForwardedRef(props: Ancho
   const { id, href, children, className, target = '_self' } = props;
   const external = target === '_blank';
   const { hoverRef, isHovered } = useHover<HTMLAnchorElement>(ref as React.RefObject<HTMLAnchorElement>);
+  const anchorClasses = classNames(AnchorLinkStyles.anchorlink, className ?? '');
   return (
-    <StyledAnchorLink id={id} href={href} className={className} target={target} external={external} ref={hoverRef}>
+    <a id={id} href={href} className={anchorClasses} target={target} ref={hoverRef}>
       {children}
       {external && (
         <Icon
+          className={AnchorLinkStyles.anchorlink__icon}
           svgIcon={ArrowUpRight}
           color={getColor('blueberry', 600)}
           hoverColor={getColor('blueberry', 700)}
-          size={48}
+          size={40}
           isHovered={isHovered}
         />
       )}
-    </StyledAnchorLink>
+    </a>
   );
 });
 
