038ceb0d83f6bf988858623cd1784b0fc0833364
Frederic Hermandsen <frederic.hermandsen@nhn.no>
2021-02-23 17:14:36 +0100

    fjernet ubrukt import

--- npm/designsystem/src/components/AnchorLink/AnchorLink.tsx
+++ npm/designsystem/src/components/AnchorLink/AnchorLink.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Icon from '../Icons';
 import ArrowUpRight from '../Icons/ArrowUpRight';
 import { getColor } from '../../theme/currys';
