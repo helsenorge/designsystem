c07294c06ca125a7abbc33b8b1a87d38c222fa6e
Frederic Hermandsen <Frederic.Hermandsen@nhn.no>
2021-10-15 10:36:28 +0000

    Merged PR 45250: feature: nye ikoner
    Nye ikoner
    - Birthcontrol
    - Gallery
    - List
    - STDs
    
    Related work items: #261843

---
+++ npm/designsystem/src/components/Icons/Gallery.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { SvgPathProps } from './Icon';
+
+const Gallery: React.FC<SvgPathProps> = ({ isExtraSmall, isHovered }: SvgPathProps): JSX.Element => {
+  const normal = (
+    <path
+      fillRule={'evenodd'}
+      d="M29.141 38.174h9.031v-9.032h-9.031v9.032zm-1.8 1.799h12.632V27.341H27.341v12.632zM9.828 38.174h9.031v-9.032H9.828v9.032zm-1.8 1.799H20.66V27.341H8.028v12.632zm21.113-21.115h9.031V9.826h-9.031v9.032zm-1.8 1.8h12.632V8.026H27.341v12.632zm-17.513-1.8h9.031V9.826H9.828v9.032zm-1.8 1.8H20.66V8.026H8.028v12.632z"
+    />
+  );
+
+  const normalHover = (
+    <path
+      fillRule={'evenodd'}
+      d="M29.141 39.437h10.295V29.142H29.141v10.295zm-1.8 1.8h13.895V27.342H27.341v13.895zm-18.777-1.8h10.295V29.142H8.564v10.295zm-1.8 1.8h13.895V27.342H6.764v13.895zm1.8-22.379h10.295V8.563H8.564v10.295zm-1.8 1.8h13.895V6.763H6.764v13.895zm22.377-1.8h10.295V8.563H29.141v10.295zm-1.8 1.8h13.895V6.763H27.341v13.895z"
+    />
+  );
+
+  const simplified = (
+    <path
+      fillRule={'evenodd'}
+      d="M29.615 37.7h8.084v-8.084h-8.084V37.7zm-2.274 2.274h12.632V27.342H27.34v12.632zM10.301 37.7h8.084v-8.084h-8.084V37.7zm-2.274 2.274H20.66V27.342H8.027v12.632zm21.588-21.59h8.084V10.3h-8.084v8.084zm-2.274 2.274h12.632V8.026H27.34v12.632zm-17.04-2.274h8.084V10.3h-8.084v8.084zm-2.274 2.274H20.66V8.026H8.027v12.632z"
+    />
+  );
+
+  const simplifiedHover = (
+    <path
+      fillRule={'evenodd'}
+      d="M29.615 38.963h9.347v-9.347h-9.347v9.347zm-2.274 2.274h13.895V27.342H27.34v13.895zM9.038 38.963h9.347v-9.347H9.038v9.347zm-2.274 2.274H20.66V27.342H6.764v13.895zm2.274-22.853h9.347V9.037H9.038v9.347zm-2.274 2.274H20.66V6.763H6.764v13.895zm22.85-2.274h9.348V9.037h-9.347v9.347zm-2.273 2.274h13.895V6.763H27.34v13.895z"
+    />
+  );
+  return isExtraSmall ? (isHovered ? simplifiedHover : simplified) : isHovered ? normalHover : normal;
+};
+
+export default Gallery;
---
+++ npm/designsystem/src/components/Icons/List.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { SvgPathProps } from './Icon';
+
+const List: React.FC<SvgPathProps> = ({ isExtraSmall, isHovered }: SvgPathProps): JSX.Element => {
+  const normal = (
+    <path
+      fillRule={'evenodd'}
+      d="M9.41 32.62a1.079 1.079 0 100 2.159 1.079 1.079 0 000-2.158zm0-9.697a1.079 1.079 0 100 2.158 1.079 1.079 0 000-2.158zm0-9.702a1.079 1.079 0 100 2.158 1.079 1.079 0 000-2.158zm5.817 21.13h24.666V33.05H15.227v1.3zm0-9.7h24.666v-1.3H15.227v1.3zm0-9.697h24.665v-1.301H15.226v1.3z"
+    />
+  );
+
+  const normalHover = (
+    <path
+      fillRule={'evenodd'}
+      d="M9.41 34.62a1.079 1.079 0 100 2.159 1.079 1.079 0 000-2.158zm0-11.697a1.079 1.079 0 100 2.158 1.079 1.079 0 000-2.158zm0-11.702a1.079 1.079 0 100 2.158 1.079 1.079 0 000-2.158zm5.817 25.13h24.666V35.05H15.227v1.3zm0-11.7h24.666v-1.3H15.227v1.3zm0-11.697h24.665v-1.301H15.226v1.3z"
+    />
+  );
+
+  const simplified = (
+    <path
+      fillRule={'evenodd'}
+      d="M11.25 31.336a1.137 1.137 0 100 2.275 1.137 1.137 0 000-2.275zm0-8.474a1.137 1.137 0 100 2.275 1.137 1.137 0 000-2.275zm0-8.477a1.137 1.137 0 100 2.275 1.137 1.137 0 000-2.275zm5.085 19.226h21.552v-2.275H16.335v2.275zm0-8.474h21.552v-2.275H16.335v2.275zm0-8.475h21.552v-2.275H16.335v2.275z"
+    />
+  );
+
+  const simplifiedHover = (
+    <path
+      fillRule={'evenodd'}
+      d="M11.25 33.863a1.137 1.137 0 100 2.275 1.137 1.137 0 000-2.275zm0-11.001a1.137 1.137 0 100 2.275 1.137 1.137 0 000-2.275zm0-11a1.137 1.137 0 100 2.274 1.137 1.137 0 000-2.275zm5.084 24.277h21.553v-2.275H16.334v2.275zm0-11.002h21.553v-2.275H16.334v2.275zm0-10.997h21.553v-2.275H16.334v2.275z"
+    />
+  );
+  return isExtraSmall ? (isHovered ? simplifiedHover : simplified) : isHovered ? normalHover : normal;
+};
+
+export default List;
