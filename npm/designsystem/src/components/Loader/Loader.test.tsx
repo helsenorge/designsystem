065285a3801841e205548550fd7f6e33c391a7ff
Frederic Hermandsen <frederic.hermandsen@nhn.no>
2021-12-01 11:36:44 +0100

    test(loader): fiks loader tests

--- npm/designsystem/src/components/Loader/Loader.test.tsx
+++ npm/designsystem/src/components/Loader/Loader.test.tsx
@@ -7,29 +7,39 @@ jest.mock('uuid', () => {
   return { v4: uuidGen };
 });
 
-test('displays the loader', (): void => {
-  const { container } = render(<Loader />);
-  expect(container).toMatchSnapshot();
-});
+describe('Gitt at en loader skal vises ', (): void => {
+  describe('Når at en loader rendres ', (): void => {
+    test('Så vises loaderen', (): void => {
+      const { container } = render(<Loader />);
+      expect(container).toMatchSnapshot();
+    });
 
-test('center loader', (): void => {
-  const { container } = render(<Loader center />);
-  expect(container.firstChild).toHaveClass('center');
-});
+    test('loader has progressbar role', (): void => {
+      render(<Loader overlay />);
+      expect(screen.getByRole('progressbar')).toBeVisible();
+    });
+  });
 
-test('loader has overlay', (): void => {
-  const { container } = render(<Loader overlay />);
-  expect(container.firstChild).toHaveClass('overlay');
-  expect(container.firstChild.firstChild).toHaveAttribute('aria-labelledby', 'loader-unik-id');
-});
+  describe('Når center prop er true ', (): void => {
+    test('Så sentreres loader', (): void => {
+      const { container } = render(<Loader center />);
+      expect(container.firstChild).toHaveClass('center');
+    });
+  });
 
-test('loader has progressbar role', (): void => {
-  render(<Loader overlay />);
-  expect(screen.getByRole('progressbar')).toBeVisible();
-});
+  describe('Når overlay prop er true', (): void => {
+    test('Så har loaderen en overlay', (): void => {
+      const { container } = render(<Loader overlay />);
+      expect(container.firstChild).toHaveClass('overlay');
+      expect(container.firstChild.firstChild).toHaveAttribute('aria-labelledby', 'loader-unik-id');
+    });
+  });
 
-test('loader has external aria-labelledby', (): void => {
-  const { container } = render(<Loader ariaLabelledById="aria-test" overlay />);
-  expect(container.firstChild).toHaveClass('overlay');
-  expect(container.firstChild.firstChild).toHaveAttribute('aria-labelledby', 'aria-test');
+  describe('Når ariaLabelledById er satt', (): void => {
+    test('Så settes attributten aria-labelledby til det samme', (): void => {
+      const { container } = render(<Loader ariaLabelledById="aria-test" overlay />);
+      expect(container.firstChild).toHaveClass('overlay');
+      expect(container.firstChild.firstChild).toHaveAttribute('aria-labelledby', 'aria-test');
+    });
+  });
 });
