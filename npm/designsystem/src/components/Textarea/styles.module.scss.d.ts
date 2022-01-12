421d6f9d3978778431515fcc92bacbff83df5113
Jørgen H. Brække <jorgen.brakke@capgemini.com>
2022-01-12 15:05:52 +0100

    scss interface

--- npm/designsystem/src/components/Textarea/styles.module.scss.d.ts
+++ npm/designsystem/src/components/Textarea/styles.module.scss.d.ts
@@ -2,15 +2,15 @@ export type Styles = {
   textarea: string;
   'textarea__counter-wrapper': string;
   'textarea__counter-wrapper--invalid': string;
-  'textarea__counter-wrapper--onDark': string;
+  'textarea__counter-wrapper--OnDark': string;
   'textarea__error-text': string;
   textarea__input: string;
   'textarea__input--invalid': string;
-  'textarea__input--onBlueberry': string;
-  'textarea__input--onDark': string;
+  'textarea__input--OnBlueberry': string;
+  'textarea__input--OnDark': string;
   'textarea__input--transparent': string;
   textarea__label: string;
-  'textarea__label--onDark': string;
+  'textarea__label--OnDark': string;
   'textarea--gutterBottom': string;
 };
 
