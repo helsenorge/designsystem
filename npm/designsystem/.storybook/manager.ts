10f39131f44796f686eaadba26965cc59c620bb1
Frederic Hermandsen <Frederic.Hermandsen@nhn.no>
2022-03-25 09:29:55 +0000

    Merged PR 50740: fix: storybook fiks logo
    fix: storybook fiks logo
    
    Related work items: #275098

--- npm/designsystem/.storybook/manager.ts
+++ npm/designsystem/.storybook/manager.ts
@@ -3,8 +3,9 @@ import '@storybook/addon-actions/register';
 import '@storybook/addon-viewport/register';
 import '@storybook/addon-backgrounds/register';
 import '@storybook/addon-a11y/register';
-import {addons} from '@storybook/addons';
-import {create} from '@storybook/theming/create';
+import { addons } from '@storybook/addons';
+import { create } from '@storybook/theming/create';
+import logo from './hnd-logo.svg';
 
 /* TODO: Make use of palette from DS. Got a weird error that I think stems from the custom webpack config which crashed the build */
 const hndsTheme = create({
@@ -12,7 +13,7 @@ const hndsTheme = create({
   colorSecondary: '#188097',
   appBg: '#E4F7F9',
   brandUrl: 'https://helsenorge.design',
-  brandImage: 'https://hndsstatic.blob.core.windows.net/images/hnd-logo.png',
+  brandImage: logo,
 });
 
 addons.setConfig({
