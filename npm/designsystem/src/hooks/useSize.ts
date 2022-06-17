15ce9d2f9afbc85ee340d370cedc680bbecd9edb
Audun Rundberg <audun.rundberg@nhn.no>
2022-06-17 07:36:56 +0000

    Merged PR 53228: feat: usesize-hook fungerer uten ResizeObserver igjen
    Related work items: #272523

--- npm/designsystem/src/hooks/useSize.ts
+++ npm/designsystem/src/hooks/useSize.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { debounce } from '../utils/debounce';
 
 /**
  * Lytt på endringer i størrelse på et element
@@ -9,13 +10,34 @@ import { useEffect, useState } from 'react';
 export const useSize = (ref?: React.RefObject<HTMLElement>) => {
   const [size, setSize] = useState<DOMRect>();
   useEffect(() => {
-    const resizeObserver = new ResizeObserver(entries => {
-      setSize(entries[0].target.getBoundingClientRect());
-    });
-    if (ref?.current) {
-      resizeObserver.observe(ref?.current);
+    if (typeof ResizeObserver !== 'undefined') {
+      const resizeObserver = new ResizeObserver(entries => {
+        setSize(entries[0].target.getBoundingClientRect());
+      });
+      if (ref?.current) {
+        resizeObserver.observe(ref?.current);
+      }
+      return resizeObserver.disconnect;
+    } else if (typeof window === 'object') {
+      // For nettlesere som ikke støtter ResizeObserver (iOS 13 og lavere)
+      const handleLayoutEvent = () => {
+        ref?.current && setSize(ref.current.getBoundingClientRect());
+      };
+
+      const events = ['layoutchange', 'resize', 'orientationchange'];
+      const debounceMs = 10;
+
+      const [debouncedCallback, teardown] = debounce(handleLayoutEvent, debounceMs);
+
+      events.forEach(eventName => window.addEventListener(eventName, debouncedCallback));
+
+      debouncedCallback();
+
+      return () => {
+        teardown();
+        events.forEach(eventName => window.removeEventListener(eventName, debouncedCallback));
+      };
     }
-    return resizeObserver.disconnect;
   }, [ref]);
 
   return size;
