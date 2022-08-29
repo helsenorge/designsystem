30cbdef82b89a3354a1708b9773fdf7db5fdde82
Audun Rundberg <audun.rundberg@nhn.no>
2022-08-29 13:24:36 +0000

    Merged PR 54751: refactor: flytt notificationpanel-utils inn pga feil med rollup-bygg
    Related work items: #279887

--- npm/designsystem/src/components/NotificationPanel/NotificationPanel.tsx
+++ npm/designsystem/src/components/NotificationPanel/NotificationPanel.tsx
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { AriaAttributes } from 'react';
 import classNames from 'classnames';
 
 import styles from './styles.module.scss';
 import Close from '../Close';
 import { AnalyticsId } from '../../constants';
-import { getAriaLabelAttributes, variantToIconMap } from './utils';
 import { useUuid } from '../../hooks/useUuid';
+import { palette } from '../../theme/palette';
+import Icon from '../Icons';
+import InfoSignStroke from '../Icons/InfoSignStroke';
+import AlertSignStroke from '../Icons/AlertSignStroke';
+import AlertSignFill from '../Icons/AlertSignFill';
 
 export type NotificationPanelVariants = 'info' | 'warn' | 'alert' | 'crisis';
 export type NotificationPanelSizes = 'small' | 'medium' | 'large';
+
+const variantToIconMap = {
+  info: <Icon svgIcon={InfoSignStroke} color={palette.kiwi900} hoverColor={palette.kiwi900} />,
+  warn: <Icon svgIcon={AlertSignStroke} color="black" hoverColor="black" />,
+  alert: <Icon svgIcon={AlertSignFill} color={palette.cherry500} hoverColor={palette.cherry500} />,
+  alertLabel: <Icon svgIcon={AlertSignStroke} color={palette.cherry500} hoverColor={palette.cherry500} />,
+  crisis: <Icon svgIcon={AlertSignFill} color={palette.banana200} hoverColor={palette.banana200} />,
+};
+
+interface AriaLabelAttributesConfig {
+  label?: string;
+  id?: string;
+}
+
+type AriaLabelAttributes = Pick<AriaAttributes, 'aria-label' | 'aria-labelledby'>;
+
+export const getAriaLabelAttributes = (config: AriaLabelAttributesConfig): AriaLabelAttributes | undefined => {
+  const { label, id } = config;
+
+  if (id) {
+    return {
+      'aria-labelledby': id,
+    };
+  }
+  if (label) {
+    return {
+      'aria-label': label,
+    };
+  }
+};
 
 interface NotificationPanelProps {
   /** Adds custom classes to the element. */
