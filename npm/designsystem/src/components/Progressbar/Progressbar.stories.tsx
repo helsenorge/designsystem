c35fee1a0dd2bf9e9f111f1d0a69ed687b01d238
Audun Rundberg <audun@rundberg.no>
2022-10-17 12:32:20 +0200

    docs: bruk grid i stories for progressbar

--- npm/designsystem/src/components/Progressbar/Progressbar.stories.tsx
+++ npm/designsystem/src/components/Progressbar/Progressbar.stories.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
+import Grid from '../GridExample';
+
 import Progressbar from './Progressbar';
 
 export default {
@@ -14,30 +16,27 @@ export default {
     },
     ariaLabelledById: {
       control: 'text',
-      defaultValue: '',
     },
     value: {
       control: 'number',
-      defaultValue: 1,
     },
     min: {
       control: 'number',
-      defaultValue: 1,
     },
     max: {
       control: 'number',
-      defaultValue: 20,
     },
   },
 } as ComponentMeta<typeof Progressbar>;
 
-export const Default: ComponentStory<typeof Progressbar> = (args: any) => (
-  <div style={{ width: '20rem' }}>
-    <Progressbar {...args} />
-  </div>
-);
 export const Dots: ComponentStory<typeof Progressbar> = (args: any) => (
-  <div style={{ width: '20rem' }}>
-    <Progressbar {...args} min={1} max={5} />
-  </div>
+  <Grid>
+    <Progressbar {...args} />
+  </Grid>
+);
+
+export const Smooth: ComponentStory<typeof Progressbar> = (args: any) => (
+  <Grid>
+    <Progressbar min={1} max={100} {...args} />
+  </Grid>
 );
