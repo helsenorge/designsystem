62dcd0fc158d3699f51446c3304907a6ca4aa976
Frederic Hermandsen <frederic.hermandsen@nhn.no>
2023-02-02 13:20:42 +0100

    fix: login icon fill-rule fikset

--- npm/designsystem/src/components/Icons/Login.tsx
+++ npm/designsystem/src/components/Icons/Login.tsx
@@ -6,9 +6,9 @@ const Login: React.FC<SvgPathProps> = ({ size, isHovered }: SvgPathProps): JSX.E
     <>
       <path d="M18.05 20.5v-9.39l16.4-2.589v30.957l-16.4-2.589v-8.39h-1.3v8.646a1 1 0 0 0 .844.988l17 2.684a1 1 0 0 0 1.156-.987V8.17a1 1 0 0 0-1.156-.988l-17 2.685a1 1 0 0 0-.844.987V20.5h1.3Z" />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M26.673 24.936H8.188v-1.3h18.449l-3.778-3.779.919-.92 4.907 4.906a.653.653 0 0 1 0 .92l-4.971 4.971-.919-.92 3.878-3.878Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </>
   );
@@ -17,9 +17,9 @@ const Login: React.FC<SvgPathProps> = ({ size, isHovered }: SvgPathProps): JSX.E
     <>
       <path d="M18.05 20.5v-9.39l16.4-2.589v30.957l-16.4-2.589v-8.39h-1.3v8.646a1 1 0 0 0 .844.988l17 2.684a1 1 0 0 0 1.156-.987V8.17a1 1 0 0 0-1.156-.988l-17 2.685a1 1 0 0 0-.844.987V20.5h1.3Z" />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M29.673 24.936H11.188v-1.3h18.449l-3.778-3.779.919-.92 4.907 4.906a.653.653 0 0 1 0 .92l-4.971 4.971-.919-.92 3.878-3.878Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </>
   );
@@ -28,9 +28,9 @@ const Login: React.FC<SvgPathProps> = ({ size, isHovered }: SvgPathProps): JSX.E
     <>
       <path d="M18.392 20.5v-9.097l15.716-2.481v30.156l-15.716-2.481V28.5H16.75v8.42c0 .622.452 1.151 1.066 1.248L34.29 40.77a1.263 1.263 0 0 0 1.46-1.248V8.478c0-.776-.693-1.368-1.46-1.247l-16.474 2.6a1.263 1.263 0 0 0-1.066 1.248V20.5h1.642Z" />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M26.673 24.936H8.188v-1.3h18.449l-3.778-3.779.919-.92 4.907 4.906a.653.653 0 0 1 0 .92l-4.971 4.971-.919-.92 3.878-3.878Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </>
   );
@@ -39,9 +39,9 @@ const Login: React.FC<SvgPathProps> = ({ size, isHovered }: SvgPathProps): JSX.E
     <>
       <path d="M18.392 20.5v-9.097l15.716-2.481v30.156l-15.716-2.481V28.5H16.75v8.42c0 .622.452 1.151 1.066 1.248L34.29 40.77a1.263 1.263 0 0 0 1.46-1.248V8.478c0-.776-.693-1.368-1.46-1.247l-16.474 2.6a1.263 1.263 0 0 0-1.066 1.248V20.5h1.642Z" />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M29.673 24.936H11.188v-1.3h18.449l-3.778-3.779.919-.92 4.907 4.906a.653.653 0 0 1 0 .92l-4.971 4.971-.919-.92 3.878-3.878Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </>
   );
--- npm/designsystem/src/components/Icons/TriangleX.tsx
+++ npm/designsystem/src/components/Icons/TriangleX.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { getIcon, SvgPathProps } from './Icon';
 
-const TrangleX: React.FC<SvgPathProps> = ({ size, isHovered }: SvgPathProps): JSX.Element => {
+const TriangleX: React.FC<SvgPathProps> = ({ size, isHovered }: SvgPathProps): JSX.Element => {
   const normal = (
     <>
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="m24 7 19 33.712H5L24 7Zm-5.792 17.951a1.185 1.185 0 1 1 1.676-1.675l4.01 4.01 3.895-3.895a1.19 1.19 0 0 1 1.683 1.683l-3.895 3.895 4.387 4.387a1.185 1.185 0 0 1-1.675 1.675l-4.387-4.386-4.503 4.502a1.19 1.19 0 0 1-1.683-1.683l4.503-4.502-4.01-4.011Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </>
   );
@@ -15,9 +15,9 @@ const TrangleX: React.FC<SvgPathProps> = ({ size, isHovered }: SvgPathProps): JS
   const normalHover = (
     <>
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="m24 7 19 33.712H5L24 7Zm-4.243 19.433a1.185 1.185 0 0 1 1.676-1.676l2.563 2.564 2.564-2.564a1.19 1.19 0 0 1 1.683 1.683l-2.564 2.564 2.564 2.563a1.184 1.184 0 1 1-1.676 1.676l-2.563-2.564-2.564 2.564a1.19 1.19 0 1 1-1.683-1.683l2.564-2.564-2.564-2.563Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </>
   );
@@ -25,9 +25,9 @@ const TrangleX: React.FC<SvgPathProps> = ({ size, isHovered }: SvgPathProps): JS
   const xSmall = (
     <>
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="m24 7.5 19 33.712H5L24 7.5Zm-5.792 17.95a1.185 1.185 0 0 1 1.676-1.676l4.01 4.01 3.895-3.894a1.19 1.19 0 1 1 1.683 1.683l-3.895 3.895 4.387 4.387a1.184 1.184 0 1 1-1.675 1.675l-4.387-4.387-4.502 4.502a1.19 1.19 0 1 1-1.683-1.683l4.502-4.502-4.01-4.01Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </>
   );
@@ -35,9 +35,9 @@ const TrangleX: React.FC<SvgPathProps> = ({ size, isHovered }: SvgPathProps): JS
   const xSmallHover = (
     <>
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="m24 7.5 19 33.712H5L24 7.5Zm-4.243 19.433a1.185 1.185 0 0 1 1.676-1.675l2.563 2.564 2.564-2.564a1.19 1.19 0 0 1 1.683 1.683l-2.564 2.564 2.564 2.563a1.185 1.185 0 0 1-1.676 1.675l-2.563-2.563-2.564 2.564a1.19 1.19 0 1 1-1.683-1.683l2.564-2.564-2.564-2.564Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </>
   );
@@ -45,4 +45,4 @@ const TrangleX: React.FC<SvgPathProps> = ({ size, isHovered }: SvgPathProps): JS
   return getIcon(size, isHovered, normal, normalHover, xSmall, xSmallHover);
 };
 
-export default TrangleX;
+export default TriangleX;
