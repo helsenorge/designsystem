5be5ed35a13fad559e400c2ecae5f439e2bb48fd
Audun Rundberg <audun.rundberg@nhn.no>
2023-09-14 12:01:34 +0000

    Merged PR 67318: feat: step støtter sticky knapper
    Related work items: #307046

--- npm/designsystem/src/components/Step/Step.tsx
+++ npm/designsystem/src/components/Step/Step.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import classNames from 'classnames';
 
+import { useSticky } from '../../hooks/useSticky';
 import { ButtonProps } from '../Button';
 import { StepperProps } from '../Stepper';
 
@@ -20,14 +21,51 @@ interface StepProps {
   additionalButtons?: React.ReactElement<ButtonProps>[];
   /** Knapp for å avbryte eller fortsette senere. Vises med "borderless" variant. */
   cancelButton?: React.ReactElement<ButtonProps>;
+  /** Knappene vil vises sticky nederst på skjermen dersom innholdet i Step tar mer plass enn vinduet. Default: false  */
+  stickyButtons?: boolean;
 }
 
-const Step: React.FC<StepProps> = ({ stepper, children, backButton, forwardButton, additionalButtons, cancelButton }) => {
+const Step: React.FC<StepProps> = ({
+  stepper,
+  children,
+  backButton,
+  forwardButton,
+  additionalButtons,
+  cancelButton,
+  stickyButtons = false,
+}) => {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const navigationRef = useRef<HTMLDivElement>(null);
+  const { isOutsideWindow, offsetHeight, contentWidth } = useSticky(contentRef, navigationRef, 'bottom');
+
+  const isSticky = stickyButtons && isOutsideWindow;
+  const hasNavigation = backButton || forwardButton || additionalButtons || cancelButton;
+
+  const contentClasses = classNames(!hasNavigation && styles['step__content--no-navigation']);
+  const navigationClasses = classNames(
+    styles.step__navigation,
+    !hasNavigation && styles['step__navigation--hidden'],
+    stickyButtons && styles['step__navigation--has-sticky-buttons'],
+    isSticky && styles['step__navigation--is-sticky']
+  );
+
   return (
-    <>
+    <div className={classNames(stickyButtons && styles['step--has-sticky-buttons'])}>
       {stepper && <div className={styles.step__stepper}>{stepper}</div>}
-      <div className={styles.step__content}>{children}</div>
-      <div className={styles.step__navigation}>
+      <div
+        className={contentClasses}
+        ref={contentRef}
+        style={{ paddingBottom: stickyButtons && offsetHeight ? `${offsetHeight}px` : undefined }}
+      >
+        {children}
+      </div>
+      <div
+        className={navigationClasses}
+        ref={navigationRef}
+        style={{
+          width: stickyButtons && contentWidth ? `${contentWidth}px` : undefined,
+        }}
+      >
         {(backButton || forwardButton) && (
           <div className={classNames(styles.step__buttons, styles['step__buttons--navigation'])}>
             {backButton &&
@@ -53,7 +91,7 @@ const Step: React.FC<StepProps> = ({ stepper, children, backButton, forwardButto
           </div>
         )}
       </div>
-    </>
+    </div>
   );
 };
 
