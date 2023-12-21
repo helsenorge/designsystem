9bbc8ea0d2904a33af20bff82c4b227728d468f8
Audun Rundberg <audun.rundberg@nhn.no>
2023-12-21 12:53:59 +0100

    chore: rydd i todos

--- npm/designsystem/src/components/Badge/Badge.test.tsx
+++ npm/designsystem/src/components/Badge/Badge.test.tsx
@@ -4,29 +4,23 @@ import { render, screen } from '@testing-library/react';
 
 import Badge from './Badge';
 
-const badgeNumber = 123;
-
-test('displays text', (): void => {
-  const { container } = render(<Badge>{badgeNumber}</Badge>);
-  expect(screen.getByText(badgeNumber.toString())).toBeInTheDocument();
-  expect(container).toMatchSnapshot();
-});
-
-/* 
-  TODO: Waiting for issues regarding babel and types for this to be implemented
-  Seems to be version issues because of v5 release.
-*/
-// test('has the right color', (): void => {
-//   const {container} = render(<Badge color="blood200">{badgeNumber}</Badge>);
-//   expect(container).toMatchSnapshot();
-// });
-
 describe('Gitt at Badge skal vises', (): void => {
+  describe('Når Badge har tekst', (): void => {
+    test('Så vises teksten', (): void => {
+      const { container } = render(<Badge>{123}</Badge>);
+
+      const badgeNumber = screen.getByText('123');
+
+      expect(badgeNumber).toBeVisible();
+      expect(container).toMatchSnapshot();
+    });
+  });
   describe('Når testId-prop er satt', (): void => {
     test('Så kan komponenten finnes ved hjelp av testId', (): void => {
       render(<Badge testId="bare-tester">123</Badge>);
 
       const component = screen.getByTestId('bare-tester');
+
       expect(component).toBeVisible();
     });
   });
--- npm/designsystem/src/components/Icons/Cross.tsx
+++ npm/designsystem/src/components/Icons/Cross.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { SvgPathProps } from '../Icon';
 
-// TODO: This should be removed. This will cause a breaking change if people use it.
-// Want to wait with removing this untill we have more breaking changes in v1.1 for example
 const Cross: React.FC<SvgPathProps> = ({ size, isHovered }: SvgPathProps): React.ReactElement => {
   const normal = (
     <path d="M20.404 19l8.259-8.259-1.403-1.403-8.259 8.259-8.26-8.26-1.403 1.403 8.26 8.26-8.26 8.26 1.403 1.403 8.26-8.26 8.259 8.259 1.403-1.403z" />
