982642930b7963dec220ce9e62e2101e69534277
Audun Rundberg <audun.rundberg@nhn.no>
2024-02-21 16:17:07 +0100

    docs: vertikal padding

--- npm/designsystem/src/components/GridExample/GridExample.tsx
+++ npm/designsystem/src/components/GridExample/GridExample.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const GridExample: React.FC = ({ children }) => (
-  <div className="container mt-5">
+  <div className="container py-5">
     <div className="row">
       <div className="col">{children}</div>
     </div>
