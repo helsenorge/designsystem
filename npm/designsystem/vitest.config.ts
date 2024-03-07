639a9ad8cc2c7a02a1f53a120770f0ca0694b97f
audunru <audun.rundberg@nhn.no>
2024-03-07 17:54:25 +0100

    test: combine coverage

--- npm/designsystem/vitest.config.ts
+++ npm/designsystem/vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       },
     },
     coverage: {
-      reporter: ['cobertura', 'lcov', 'json'],
+      reporter: ['cobertura', 'json'],
     },
     reporters: ['junit'],
     outputFile: {
