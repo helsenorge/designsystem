d28a280d4afe56227a331334348d91aac5be6281
Audun Rundberg <audun.rundberg@nhn.no>
2024-08-23 07:06:41 +0000

    Merged PR 91248: fix: table kan brukes i tabs uten problemer med scrolling på mobil
    Related work items: #330555

---
+++ npm/designsystem/src/hooks/usestopPropagation.ts
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+
+/**
+ * Stopp eventer fra å boble opp videre i DOMen
+ * @param ref Element som skal overvåkes
+ * @param events Navn på eventer som ikke skal boble opp
+ */
+export const useStopPropagation = (ref: React.RefObject<HTMLElement>, events: string[]) => {
+  const handleEvent = (e: Event): void => e.stopPropagation();
+
+  useEffect(() => {
+    events.forEach(name => ref?.current?.addEventListener(name, handleEvent));
+
+    return (): void => events.forEach(name => ref.current?.removeEventListener(name, handleEvent));
+  }, [ref]);
+};
