3ac0b8822820ad47ac635da95b7d7377481f2f33
Audun Rundberg <audun.rundberg@nhn.no>
2024-08-23 09:17:16 +0000

    Merged PR 91252: fix: validationsummary er ikke labelled by ikke-eksisterende element
    Related work items: #328895

---
+++ npm/designsystem/src/components/Validation/ValidationSummary.test.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { render, screen, within } from '@testing-library/react';
+
+import ValidationSummary from './ValidationSummary';
+
+describe('Gitt at ValidationSummary skal vises', () => {
+  describe('Når ValidationSummary ikke har noen props', () => {
+    test('Så vises en tom alert', () => {
+      render(<ValidationSummary />);
+
+      const alert = screen.getByRole('alert');
+
+      expect(alert).not.toHaveAccessibleName();
+      expect(alert).toBeEmptyDOMElement();
+    });
+  });
+
+  describe('Når ValidationSummary har title, men ikke errors', () => {
+    test('Så vises en tom alert', () => {
+      render(<ValidationSummary errorTitle="Feil" />);
+
+      const alert = screen.getByRole('alert');
+
+      expect(alert).not.toHaveAccessibleName();
+      expect(alert).toBeEmptyDOMElement();
+    });
+  });
+
+  describe('Når ValidationSummary har title og errors', () => {
+    test('Så vises en tom alert', () => {
+      render(<ValidationSummary errorTitle="Feil" errors={{ feil1: { message: 'Feil 1' } }} />);
+
+      const alert = screen.getByRole('alert', { name: 'Feil' });
+      within(alert).getByText('Feil 1');
+    });
+  });
+
+  describe('Når ValidationSummary har children', () => {
+    test('Så vises teksten', () => {
+      render(<ValidationSummary>{'Feilmelding'}</ValidationSummary>);
+
+      const alert = screen.getByRole('alert');
+
+      within(alert).getByText('Feilmelding');
+    });
+  });
+});
