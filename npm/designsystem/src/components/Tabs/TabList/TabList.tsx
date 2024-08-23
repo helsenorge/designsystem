2bddf2017f1067ba1b17473006b2103de630054e
Audun Rundberg <audun.rundberg@nhn.no>
2024-08-23 10:21:31 +0000

    Merged PR 91312: fix: unngå blinking i tabs på mobil når man scroller
    Related work items: #330597

--- npm/designsystem/src/components/Tabs/TabList/TabList.tsx
+++ npm/designsystem/src/components/Tabs/TabList/TabList.tsx
@@ -2,14 +2,10 @@ import React, { useRef } from 'react';
 
 import classNames from 'classnames';
 
-import { useIsVisible } from '../../../hooks/useIsVisible';
+import TabItem from './TabItem';
 import { useRovingFocus } from '../../../hooks/useRovingFocus';
-import { palette } from '../../../theme/palette';
 import { isComponent } from '../../../utils/component';
-import Icon, { IconSize } from '../../Icon';
-import { IconName } from '../../Icons/IconNames';
-import LazyIcon from '../../LazyIcon';
-import Tab from '../Tab';
+import Tab, { TabProps } from '../Tab';
 import { TabsColors, TabsType } from '../Tabs';
 
 import styles from './styles.module.scss';
@@ -31,59 +27,20 @@ const TabList: React.FC<TabListProps> = props => {
 
   const tablistClasses = classNames(styles['tab-list'], styles[`tab-list--${type}`]);
 
-  const isVisible = useIsVisible(listRef);
-
   return (
     <ul className={tablistClasses} ref={listRef} role="tablist" aria-orientation="horizontal">
       {React.Children.map(children, (child, index) => {
-        if (isComponent(child, Tab)) {
-          const isSelected = index === selectedTab;
-          const { title, onTabClick, icon, testId } = child.props;
-          const handleClick = (): void => {
-            onTabClick && onTabClick(index);
-            onTabListClick(index);
-            scrollToTab(index);
-          };
-          const tabButtonClasses = classNames(styles['tab-list__tab'], styles[`tab-list__tab--${color}`], {
-            [styles['tab-list__tab--selected']]: isSelected,
-            [styles['tab-list__tab--first']]: index == 0,
-          });
-
-          const currentRef = tabRefs.current && tabRefs.current[index];
-
-          const scrollToTab = (index: number): void => {
-            const currentRef = tabRefs.current && tabRefs.current[index];
-            currentRef?.current?.scrollIntoView({ behavior: 'smooth', inline: 'center', block: 'nearest' });
-          };
-
-          if (isSelected && isVisible) scrollToTab(index);
-
+        if (isComponent<TabProps>(child, Tab)) {
           return (
-            <li role="presentation" key={`${title}`}>
-              <button
-                role="tab"
-                aria-selected={isSelected}
-                onClick={handleClick}
-                className={tabButtonClasses}
-                key={`${title}`}
-                data-testid={testId}
-                ref={currentRef as React.RefObject<HTMLButtonElement>}
-              >
-                <span className={styles['tab-list__tab__title-and-icon']}>
-                  {icon &&
-                    (typeof icon === 'string' ? (
-                      <LazyIcon
-                        iconName={icon as IconName}
-                        size={IconSize.XSmall}
-                        color={isSelected ? palette[`black`] : palette['blueberry500']}
-                      />
-                    ) : (
-                      <Icon svgIcon={icon} size={IconSize.XSmall} color={isSelected ? palette[`black`] : palette['blueberry500']} />
-                    ))}
-                  {title}
-                </span>
-              </button>
-            </li>
+            <TabItem
+              tabRefs={tabRefs}
+              key={child.props.title}
+              index={index}
+              selectedTab={selectedTab}
+              onTabListClick={onTabListClick}
+              tabProps={child.props}
+              color={color}
+            />
           );
         }
         return null;
