fe4f664f497dfc8fdd505b1d8106025f69718108
Marie Pauline Kristiansen <marie.pauline.kristiansen@nhn.no>
2024-09-25 15:12:29 +0200

    lag klasser for fade på tablist

--- npm/designsystem/src/components/Tabs/TabList/styles.module.scss.d.ts
+++ npm/designsystem/src/components/Tabs/TabList/styles.module.scss.d.ts
@@ -8,6 +8,9 @@ export type Styles = {
   'tab-list__tab--not-selected': string;
   'tab-list__tab--selected': string;
   'tab-list__tab--white': string;
+  'tab-list--fade-both': string;
+  'tab-list--fade-end': string;
+  'tab-list--fade-start': string;
   'tab-list--onblueberry': string;
   'tab-list--onneutral': string;
   'tab-list--onwhite': string;
