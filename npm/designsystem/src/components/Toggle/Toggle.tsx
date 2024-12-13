65d17c0d2472dadf5ad4f91cba09a3fc54b037ec
Frederic Hermandsen <Frederic.Hermandsen@nhn.no>
2024-12-13 13:07:06 +0000

    Merged PR 106820: chore: framer-motion har blitt motion
    chore: framer-motion har blitt motion
    
    Related work items: #337296

--- npm/designsystem/src/components/Toggle/Toggle.tsx
+++ npm/designsystem/src/components/Toggle/Toggle.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import classNames from 'classnames';
-import { useAnimate } from 'framer-motion';
+import { useAnimate } from 'motion/react';
 
 import { AnalyticsId } from '../../constants';
 import { usePseudoClasses } from '../../hooks/usePseudoClasses';
