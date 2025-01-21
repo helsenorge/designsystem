b29f80eff071cbf51e3f0a72a7e0d14bf3f1d9e2
Marie Pauline Kristiansen <marie.pauline.kristiansen@nhn.no>
2025-01-21 15:16:31 +0100

    docs: legg til badges som viser status

--- npm/designsystem/.storybook/manager.ts
+++ npm/designsystem/.storybook/manager.ts
@@ -4,6 +4,7 @@ import { create } from '@storybook/theming/create';
 import { palette } from '../src/theme/palette';
 
 import logo from './frankenstein-storybook.svg';
+import { defaultConfig, type TagBadgeParameters } from 'storybook-addon-tag-badges';
 
 const theme = create({
   base: 'light',
@@ -15,4 +16,43 @@ const theme = create({
 
 addons.setConfig({
   theme,
+  tagBadges: [
+    {
+      tags: 'new',
+      badge: {
+        text: 'New',
+        bgColor: palette.blueberry100,
+        fgColor: palette.blueberry900,
+      },
+      display: {
+        sidebar: ['component'],
+        toolbar: true,
+      },
+    },
+    {
+      tags: /breaking(-change|-changes)?/,
+      badge: {
+        text: 'Breaking changes',
+        bgColor: palette.banana100,
+        fgColor: palette.banana900,
+      },
+      display: {
+        sidebar: ['component'],
+        toolbar: true,
+      },
+    },
+    {
+      tags: 'deprecated',
+      badge: {
+        text: 'Deprecated',
+        bgColor: palette.cherry100,
+        fgColor: palette.cherry900,
+      },
+      display: {
+        sidebar: ['component'],
+        toolbar: true,
+      },
+    },
+    ...defaultConfig,
+  ] satisfies TagBadgeParameters,
 });
