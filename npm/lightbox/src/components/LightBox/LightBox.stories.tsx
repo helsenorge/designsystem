03be088339e5114656146558069711ffd174e913
Marie Pauline Kristiansen <marie.pauline.kristiansen@nhn.no>
2025-02-12 08:17:30 +0100

    docs: legg til eksempel med scrollbar bak

--- npm/lightbox/src/components/LightBox/LightBox.stories.tsx
+++ npm/lightbox/src/components/LightBox/LightBox.stories.tsx
@@ -4,6 +4,9 @@ import { action } from '@storybook/addon-actions';
 import { StoryObj, Meta } from '@storybook/react';
 import { Docs } from 'frankenstein-build-tools';
 
+import Button from '@helsenorge/designsystem-react/components/Button';
+import longLoremText from '@helsenorge/designsystem-react/utils/loremtext';
+
 import LightBox from './LightBox';
 
 const meta = {
@@ -57,4 +60,42 @@ export const LangBildetekst: Story = {
       closeTextAfterSeconds={undefined}
     />
   ),
+};
+
+export const MedStuffBak: Story = {
+  render: args => {
+    const [isOpen, setIsOpen] = React.useState(false);
+    return (
+      <div>
+        <Button onClick={() => setIsOpen(!isOpen)}>{'Åpne lightbox'}</Button>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        <p>{longLoremText}</p>
+        {isOpen && (
+          <LightBox
+            {...args}
+            imageText={
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu turpis posuere, dignissim ex at, interdum metus. Etiam efficitur ut lectus et condimentum. Suspendisse ornare suscipit metus sit amet luctus. Quisque risus orci, molestie sit amet tempus non, semper a ligula. Etiam volutpat scelerisque magna vel feugiat. Sed ac venenatis justo. Aliquam iaculis ante a eros sagittis, id gravida felis placerat. Cras luctus mi quam, non venenatis lorem condimentum vel. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer nulla sem, placerat non blandit ac, interdum vel augue. Nulla fermentum orci non augue pulvinar, sed posuere neque scelerisque. Aenean pulvinar commodo lorem vel consectetur. Nam augue lectus, tempus vitae finibus id, dignissim id eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pulvinar commodo lorem vel consectetur. Nam augue lectus, tempus vitae finibus id, dignissim id eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            }
+            closeTextAfterSeconds={undefined}
+            onClose={() => setIsOpen(false)}
+          />
+        )}
+      </div>
+    );
+  },
 };
