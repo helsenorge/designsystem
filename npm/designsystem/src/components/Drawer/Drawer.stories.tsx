133314766290088e8e93be0db7608803e3f20e0c
Frederic Hermandsen <Frederic.Hermandsen@nhn.no>
2025-02-12 12:58:21 +0000

    Merged PR 114630: docs(drawer): tar i bruk storybook useargs for controller synkronisering med state
    docs(drawer): tar i bruk useargs for controller synkronisering med state
    
    Related work items: #339100

--- npm/designsystem/src/components/Drawer/Drawer.stories.tsx
+++ npm/designsystem/src/components/Drawer/Drawer.stories.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { action } from '@storybook/addon-actions';
 import { Source } from '@storybook/blocks';
+import { useArgs } from '@storybook/preview-api';
 import { StoryObj, Meta } from '@storybook/react';
 import { Docs } from 'frankenstein-build-tools';
 
@@ -21,8 +22,8 @@ const meta = {
           belowControlsContent={
             <>
               <section style={{ margin: '1rem 0' }}>
-                <h2>{'Rendering Drawer conditionally'}</h2>
-                <p>{'Use the isOpen prop to render the drawer conditionally'}</p>
+                <h2>{'Rendring av Drawer'}</h2>
+                <p>{'Bruk isOpen-proppen sammen med onRequestClose-callbacken for å styre rendringen av Drawer.'}</p>
                 <Source
                   language="tsx"
                   code={`
@@ -32,7 +33,8 @@ const meta = {
   const [isOpen, setIsOpen] = useState(true);
 
   <Drawer
-    isOpen={setIsOpen(false)}
+    isOpen={isOpen}
+    onRequestClose={setIsOpen(false)}
     // ...other props
   >
     {/* Drawer content here */}
@@ -54,48 +56,32 @@ const meta = {
     },
   },
   args: {
-    title: 'Fullstendige bruksvilkår',
     children:
       'Cillum officia officia elit et officia eu eiusmod eu exercitation irure exercitation reprehenderit. Ut irure eiusmod ad Lorem nostrud Lorem ut. Ullamco deserunt dolore deserunt voluptate Lorem occaecat. Commodo id adipisicing sint adipisicing ut. Labore consequat commodo laboris excepteur deserunt qui sunt veniam elit cupidatat. Qui sit officia nostrud et dolore adipisicing et ut nostrud eu. Sint ullamco ullamco laborum nostrud nulla velit et sit excepteur qui sit proident non. Aute in ea sint ipsum commodo commodo veniam. Labore sunt sint cillum sit magna tempor non nulla. Aliquip tempor eu excepteur ea exercitation dolor ea incididunt. Proident deserunt nisi incididunt minim mollit sit incididunt et excepteur voluptate dolor est cillum. Sit cupidatat sunt in commodo anim minim culpa sint incididunt cupidatat. Tempor sunt nostrud voluptate mollit aliqua occaecat veniam eiusmod culpa ea velit. Lorem ad ea duis ex tempor elit sit nostrud voluptate sit eu quis. Quis elit ullamco anim occaecat amet fugiat qui consectetur. Eu id deserunt tempor amet sit laboris dolor sunt. Et adipisicing id culpa enim in adipisicing in labore incididunt nisi. Minim enim dolore aute nostrud esse exercitation minim consectetur do sit deserunt. Anim ex reprehenderit culpa magna exercitation mollit eiusmod quis velit incididunt. Non minim quis fugiat aliquip quis commodo Lorem laborum proident sunt tempor irure. Dolor proident eu cupidatat duis sint. Ex consectetur laborum nisi consequat. Sit est non nostrud consequat deserunt labore irure laboris magna cupidatat elit amet.',
+    onPrimaryAction: action('Primary action clicked'),
+    onRequestClose: action('Drawer closed'),
+    onSecondaryAction: action('Secondary action clicked'),
     primaryActionText: 'Send inn',
     secondaryActionText: 'Avbryt',
-    ariaLabelCloseBtn: 'Lukk',
+    title: 'Fullstendige bruksvilkår',
   },
   argTypes: {
-    ariaLabel: {
-      control: 'text',
-    },
-    ariaLabelledBy: {
-      control: 'text',
-    },
-    ariaLabelCloseBtn: {
-      control: 'text',
-    },
-    children: {
-      control: 'text',
-    },
-    title: {
-      control: 'text',
-    },
-    titleHtmlMarkup: {
-      control: 'select',
-      options: allTitleTags,
-    },
-    onRequestClose: {
-      action: 'onRequestClose',
-    },
-    onPrimaryAction: {
-      action: 'onPrimaryAction',
-    },
-    onSecondaryAction: {
-      action: 'onSecondaryAction',
-    },
-    primaryActionText: {
-      control: 'text',
-    },
-    secondaryActionText: {
-      control: 'text',
-    },
+    ariaLabel: { control: 'text' },
+    ariaLabelCloseBtn: { control: 'text' },
+    ariaLabelledBy: { control: 'text' },
+    children: { control: 'text' },
+    desktopDirection: { control: 'select', options: ['left', 'right'] },
+    footerContent: { control: 'object' },
+    isOpen: { control: 'boolean' },
+    onPrimaryAction: { action: 'onPrimaryAction' },
+    onRequestClose: { action: 'onRequestClose' },
+    onSecondaryAction: { action: 'onSecondaryAction' },
+    primaryActionText: { control: 'text' },
+    secondaryActionText: { control: 'text' },
+    title: { control: 'text' },
+    titleHtmlMarkup: { control: 'select', options: allTitleTags },
+    titleId: { control: 'text' },
+    zIndex: { control: 'number' },
   },
 } satisfies Meta<typeof Drawer>;
 
@@ -108,10 +94,11 @@ export const Default: Story = {
     onRequestClose: action('Drawer closed'),
     onPrimaryAction: action('Primary action clicked'),
     onSecondaryAction: action('Secondary action clicked'),
+    isOpen: true,
   },
 
   render: (args: DrawerProps) => {
-    const [isOpen, setIsOpen] = useState(true);
+    const [{ isOpen }, setIsOpen] = useArgs<DrawerProps>();
 
     return (
       <Drawer
@@ -119,15 +106,15 @@ export const Default: Story = {
         isOpen={isOpen}
         onRequestClose={() => {
           args.onRequestClose?.();
-          setIsOpen(false);
+          setIsOpen({ isOpen: false });
         }}
         onPrimaryAction={() => {
           args.onPrimaryAction?.();
-          setIsOpen(false);
+          setIsOpen({ isOpen: false });
         }}
         onSecondaryAction={() => {
           args.onSecondaryAction?.();
-          setIsOpen(false);
+          setIsOpen({ isOpen: false });
         }}
       >
         {args.children}
@@ -141,24 +128,25 @@ export const OpenFromButton: Story = {
     onRequestClose: action('Drawer closed'),
     onPrimaryAction: action('Primary action clicked'),
     onSecondaryAction: action('Secondary action clicked'),
+    isOpen: false,
   },
 
   render: (args: DrawerProps) => {
-    const [showDrawer, setShowDrawer] = useState(false);
+    const [{ isOpen }, setIsOpen] = useArgs<DrawerProps>();
 
     return (
       <>
-        <Button onClick={() => setShowDrawer(true)}>{'Åpne Drawer'}</Button>
+        <Button onClick={() => setIsOpen({ isOpen: true })}>{'Åpne Drawer'}</Button>
         <Drawer
           {...args}
-          isOpen={showDrawer}
+          isOpen={isOpen}
           onRequestClose={() => {
             args.onRequestClose?.();
-            setShowDrawer(false);
+            setIsOpen({ isOpen: false });
           }}
           onPrimaryAction={() => {
             args.onPrimaryAction?.();
-            setShowDrawer(false);
+            setIsOpen({ isOpen: false });
           }}
           onSecondaryAction={() => {
             args.onSecondaryAction?.();
@@ -176,23 +164,24 @@ export const OpenFromButton: Story = {
 export const FooterContent: Story = {
   args: {
     onRequestClose: action('Drawer closed'),
+    isOpen: true,
   },
 
   render: (args: DrawerProps) => {
-    const [showDrawer, setShowDrawer] = useState(true);
+    const [{ isOpen }, setIsOpen] = useArgs<DrawerProps>();
 
     return (
       <Drawer
         {...args}
-        isOpen={showDrawer}
+        isOpen={isOpen}
         onRequestClose={() => {
           args.onRequestClose?.();
-          setShowDrawer(false);
+          setIsOpen({ isOpen: false });
         }}
         footerContent={
           <>
-            <Button onClick={() => setShowDrawer(false)}>{'Custom button 1'}</Button>
-            <Button onClick={() => setShowDrawer(false)} variant="outline">
+            <Button onClick={() => setIsOpen({ isOpen: false })}>{'Custom button 1'}</Button>
+            <Button onClick={() => setIsOpen({ isOpen: false })} variant="outline">
               {'Custom button 2'}
             </Button>
           </>
