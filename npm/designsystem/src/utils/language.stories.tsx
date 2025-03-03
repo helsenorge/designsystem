bbd6d1eb9e5b06780e7ce29d631b83a3f5d01e89
Audun Rundberg <audun.rundberg@nhn.no>
2025-03-03 08:08:26 +0000

    Merged PR 117498: feat(utils): felles språkkontekst
    Related work items: #345767

---
+++ npm/designsystem/src/utils/language.stories.tsx
@@ -0,0 +1,168 @@
+import React, { useState } from 'react';
+
+import { Meta, StoryObj } from '@storybook/react';
+
+import LanguageProvider, { useLanguage } from './language';
+import Button from '../components/Button';
+import Spacer from '../components/Spacer';
+import Title from '../components/Title';
+import { LanguageLocales } from '../constants';
+
+// Eksempel på bruk av context:
+
+interface Resources {
+  title: string;
+  description: string;
+}
+
+const norwegian: Resources = {
+  title: 'Eksempeltekst på norsk',
+  description: 'Beskrivelse',
+};
+
+const english: Resources = {
+  title: 'Example text in English',
+  description: 'Description',
+};
+
+const getResources = (language: LanguageLocales): Resources => {
+  switch (language) {
+    case LanguageLocales.ENGLISH:
+      return english;
+    case LanguageLocales.NORWEGIAN:
+    default:
+      return norwegian;
+  }
+};
+
+interface ComponentWithTranslationsProps {
+  resources?: Partial<Resources>;
+}
+
+const ComponentWithTranslations: React.FC<ComponentWithTranslationsProps> = ({ resources }) => {
+  const { language } = useLanguage<LanguageLocales>(LanguageLocales.NORWEGIAN);
+  const defaultResources = getResources(language);
+
+  const mergedResources: Resources = {
+    ...defaultResources,
+    ...resources,
+  };
+
+  return (
+    <div>
+      <Title>{mergedResources.title}</Title>
+      <p>{mergedResources.description}</p>
+    </div>
+  );
+};
+
+// Eksempel på bruk av provider:
+
+const LanguageProviderExample = (): React.JSX.Element => {
+  const [language, setLanguage] = useState<LanguageLocales>(LanguageLocales.NORWEGIAN);
+
+  return (
+    <LanguageProvider<LanguageLocales> language={language}>
+      <Button
+        onClick={() => setLanguage(language === LanguageLocales.ENGLISH ? LanguageLocales.NORWEGIAN : LanguageLocales.ENGLISH)}
+        variant="outline"
+      >
+        {language === LanguageLocales.ENGLISH ? 'Bytt til bokmål' : 'Switch to English'}
+      </Button>
+      <Spacer />
+      <ComponentWithTranslations />
+    </LanguageProvider>
+  );
+};
+
+const meta = {
+  title: '@helsenorge/designsystem-react/Utilities/LanguageProvider',
+  component: LanguageProvider,
+  parameters: {
+    docs: {
+      description: {
+        component: 'React Context for å sørge for at ulike komponenter bruker samme språk.',
+      },
+      source: {
+        language: 'tsx',
+        code: `
+// Eksempel på bruk av context:
+
+interface Resources {
+  title: string;
+  description: string;
+}
+
+const norwegian: Resources = {
+  title: 'Eksempeltekst på norsk',
+  description: 'Beskrivelse',
+};
+
+const english: Resources = {
+  title: 'Example text in English',
+  description: 'Description',
+};
+
+const getResources = (language: LanguageLocales): Resources => {
+  switch (language) {
+    case LanguageLocales.ENGLISH:
+      return english;
+    case LanguageLocales.NORWEGIAN:
+    default:
+      return norwegian;
+  }
+};
+
+interface ComponentWithTranslationsProps {
+  resources?: Partial<Resources>;
+}
+
+const ComponentWithTranslations: React.FC<ComponentWithTranslationsProps> = ({ resources }) => {
+  const { language } = useLanguage<LanguageLocales>(LanguageLocales.NORWEGIAN);
+  const defaultResources = getResources(language);
+
+  const mergedResources: Resources = {
+    ...defaultResources,
+    ...resources,
+  };
+
+  return (
+    <div>
+      <Title>{mergedResources.title}</Title>
+      <p>{mergedResources.description}</p>
+    </div>
+  );
+};
+
+// Eksempel på bruk av provider:
+
+const LanguageProviderExample = (): React.JSX.Element => {
+  const [language, setLanguage] = useState<LanguageLocales>(LanguageLocales.NORWEGIAN);
+
+  return (
+    <LanguageProvider<LanguageLocales> language={language}>
+      <Button
+        onClick={() => setLanguage(language === LanguageLocales.ENGLISH ? LanguageLocales.NORWEGIAN : LanguageLocales.ENGLISH)}
+        variant="outline"
+      >
+        {language === LanguageLocales.ENGLISH ? 'Bytt til bokmål' : 'Switch to English'}
+      </Button>
+      <Spacer />
+      <ComponentWithTranslations />
+    </LanguageProvider>
+  );
+};
+        `,
+      },
+    },
+    chromatic: { disableSnapshot: true },
+  },
+} satisfies Meta<typeof LanguageProvider>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => <LanguageProviderExample />,
+};
---
+++ npm/designsystem/src/utils/language.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useContext, useMemo } from 'react';
+
+interface LanguageContextType<T extends string = string> {
+  language: T;
+}
+
+const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
+
+/**
+ * Hent nåværende språk fra context
+ * @param fallbackLanguage Språk som brukes som fallback dersom hooken brukes utenfor contexten
+ * @returns LanguageContext
+ */
+export const useLanguage = <T extends string>(fallbackLanguage: T): LanguageContextType<T> => {
+  const context = useContext(LanguageContext as unknown as React.Context<LanguageContextType<T>>);
+
+  if (!context) {
+    return { language: fallbackLanguage };
+  }
+
+  return context;
+};
+
+interface LanguageProviderProps<T extends string> {
+  children: React.ReactNode;
+  language: T;
+}
+
+const LanguageProvider = <T extends string>({ children, language }: LanguageProviderProps<T>): React.JSX.Element => {
+  const value = useMemo<LanguageContextType<T>>(
+    () => ({
+      language,
+    }),
+    [language]
+  );
+
+  return <LanguageContext.Provider value={value}>{children}</LanguageContext.Provider>;
+};
+
+export default LanguageProvider;
