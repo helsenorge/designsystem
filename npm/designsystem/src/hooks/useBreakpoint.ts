00b792639a436a876a2a9f3ecdf2679b52304f54
Audun Rundberg (Innleid) <audun.rundberg@nhn.no>
2025-03-20 11:36:06 +0000

    Merged PR 123621: fix: useBreakpoint kaller ikke window.matchMedia ved server side rendering
    Related work items: #347898

--- npm/designsystem/src/hooks/useBreakpoint.ts
+++ npm/designsystem/src/hooks/useBreakpoint.ts
@@ -28,7 +28,9 @@ function getCurrentBreakpoint(): Breakpoint {
 }
 
 export const useBreakpoint = (): Breakpoint => {
-  const [breakpoint, setBreakpoint] = useState<Breakpoint>(() => getCurrentBreakpoint());
+  const [breakpoint, setBreakpoint] = useState<Breakpoint>(
+    typeof window.matchMedia !== 'undefined' ? getCurrentBreakpoint() : Breakpoint.xxs
+  );
 
   useEffect(() => {
     const handleMediaQueryEvent = (event: MediaQueryListEvent): void => {
