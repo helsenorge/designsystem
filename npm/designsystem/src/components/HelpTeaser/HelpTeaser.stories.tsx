17e53a88ffcaf9ae2f0bf683a14008cc641e92cb
Marie Pauline Kristiansen <marie.pauline.kristiansen@nhn.no>
2025-05-14 11:01:43 +0200

    feat(helpteaser): ny komponent

---
+++ npm/designsystem/src/components/HelpTeaser/HelpTeaser.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { StoryObj, Meta } from '@storybook/react';
+import { Docs } from 'frankenstein-build-tools';
+
+import HelpTeaser from './HelpTeaser';
+import { allTitleTags } from '../../../.storybook/knobs';
+import longLoremText from '../../utils/loremtext';
+
+const meta = {
+  title: '@helsenorge/designsystem-react/Components/HelpTeaser',
+  component: HelpTeaser,
+  parameters: {
+    docs: {
+      description: {
+        component: 'Beskrivelse av HelpTeaser',
+      },
+      page: (): React.JSX.Element => <Docs component={HelpTeaser} />,
+    },
+  },
+  args: {
+    title: 'Mange lurer på dette',
+    children: <span>{longLoremText}</span>,
+  },
+  argTypes: {
+    title: {
+      control: 'text',
+    },
+    htmlMarkup: {
+      control: {
+        type: 'select',
+        options: ['div', 'section', 'aside', 'article'],
+      },
+    },
+    titleHtmlMarkup: {
+      control: {
+        type: 'select',
+        options: allTitleTags,
+      },
+    },
+  },
+} satisfies Meta<typeof HelpTeaser>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+  render: args => <HelpTeaser {...args} />,
+};
