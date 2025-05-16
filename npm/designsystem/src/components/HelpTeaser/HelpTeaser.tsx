806f2ab7eba0179a1592a4a92bcd56c020d3c6f7
Marie Pauline Kristiansen <marie.pauline.kristiansen@nhn.no>
2025-05-16 08:37:48 +0200

    feat(helpteaser): ny komponent

---
+++ npm/designsystem/src/components/HelpTeaser/HelpTeaser.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { HNDesignsystemInfoTeaser } from '../../resources/Resources';
+import HandWaving from '../Icons/HandWaving';
+import InfoTeaser, { InfoTeaserTags } from '../InfoTeaser';
+import { TitleTags } from '../Title';
+
+import styles from './styles.module.scss';
+
+export interface HelpTeaserProps {
+  /** What's in the box? */
+  children: React.ReactNode;
+  /** Changes the underlying element of the wrapper */
+  htmlMarkup?: InfoTeaserTags;
+  /** Resources for component */
+  resources?: Partial<HNDesignsystemInfoTeaser>;
+  /** Sets the data-testid attribute */
+  testId?: string;
+  /** Title on top of the component */
+  title: string;
+  /** Markup props for title */
+  titleHtmlMarkup?: TitleTags;
+}
+
+const HelpTeaser: React.FC<HelpTeaserProps> = props => {
+  const { children, htmlMarkup, resources, testId, title, titleHtmlMarkup } = props;
+  return (
+    <InfoTeaser
+      testId={testId}
+      htmlMarkup={htmlMarkup}
+      className={styles.helpteaser}
+      title={title}
+      titleHtmlMarkup={titleHtmlMarkup}
+      resources={resources}
+      svgIcon={HandWaving}
+      buttonClassName={styles['helpteaser__button']}
+    >
+      {children}
+    </InfoTeaser>
+  );
+};
+
+export default HelpTeaser;
---
+++ npm/designsystem/src/components/HelpTeaser/styles.module.scss.d.ts
@@ -0,0 +1,10 @@
+export type Styles = {
+  helpteaser: string;
+  helpteaser__button: string;
+};
+
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
