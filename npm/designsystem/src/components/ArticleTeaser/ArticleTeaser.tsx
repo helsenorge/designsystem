3431e8219f0e95e75547cf78a8ed5467c3972de4
Marie Pauline Kristiansen <Marie.Pauline.Kristiansen@nhn.no>
2025-05-19 12:33:09 +0000

    Merged PR 133696: feat(articleteaser): ny komponent
    Related work items: #351362

---
+++ npm/designsystem/src/components/ArticleTeaser/ArticleTeaser.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import classNames from 'classnames';
+
+import { getResources } from './resourceHelper';
+import { LanguageLocales } from '../../constants';
+import { HNDesignsystemArticleTeaser } from '../../resources/Resources';
+import { useLanguage } from '../../utils/language';
+
+import styles from './styles.module.scss';
+
+export type ArticleTeaserTags = 'div' | 'section' | 'aside' | 'article';
+
+export interface ArticleTeaserProps {
+  /** Callback for the button. Should be used for setting the expanded boolean */
+  onExpand: () => void;
+  /** What's in the box? */
+  children: React.ReactNode;
+  /** Id of the content, used to set aria-controls */
+  contentId: string;
+  /** If the component is expanded or not */
+  expanded: boolean;
+  /** For overriding the height of the collapsed teaser */
+  heightCollapsed?: string;
+  /** Changes the underlying element of the wrapper */
+  htmlMarkup?: ArticleTeaserTags;
+  /** Resources for component */
+  resources?: Partial<HNDesignsystemArticleTeaser>;
+  /** Sets the data-testid attribute */
+  testId?: string;
+}
+
+const ArticleTeaser: React.FC<ArticleTeaserProps> = props => {
+  const { onExpand, children, contentId, expanded, htmlMarkup = 'div', resources, testId } = props;
+  const WrapperTag = htmlMarkup;
+
+  const { language } = useLanguage<LanguageLocales>(LanguageLocales.NORWEGIAN);
+  const defaultResources = getResources(language);
+
+  const mergedResources: HNDesignsystemArticleTeaser = {
+    ...defaultResources,
+    ...resources,
+  };
+
+  return (
+    <WrapperTag className={classNames(styles.articleteaser)} data-testid={testId}>
+      <div
+        className={classNames(styles['articleteaser__content'], {
+          [styles['articleteaser__content--collapsed']]: !expanded,
+        })}
+        style={{
+          maxHeight: expanded ? 'none' : props.heightCollapsed,
+        }}
+      >
+        {children}
+      </div>
+      <button
+        type="button"
+        className={classNames(styles['articleteaser__button'], {
+          [styles['articleteaser__button--expanded']]: expanded,
+        })}
+        onClick={onExpand}
+        aria-expanded={expanded}
+        aria-controls={contentId}
+      >
+        {expanded ? mergedResources.expandButtonOpen : mergedResources.expandButtonClose}
+      </button>
+    </WrapperTag>
+  );
+};
+
+export default ArticleTeaser;
