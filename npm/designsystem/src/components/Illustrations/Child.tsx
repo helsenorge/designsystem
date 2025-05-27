855cc013f9938a1e0d56e04581990a8d30ba64e1
Marie Pauline Kristiansen <Marie.Pauline.Kristiansen@nhn.no>
2025-05-27 12:19:57 +0000

    Merged PR 135597: feat(illustration): nye illustrasjoner for småbarnsliv
    feat(illustration): nye illustrasjoner for småbarnsliv
    
    Related work items: #353036

---
+++ npm/designsystem/src/components/Illustrations/Child.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { SvgIllustrationProps } from '../Illustration';
+import ChildMedium from './ChildMedium';
+import { getIllustration } from '../Illustration/utils';
+
+export interface Palette {
+  hair: string;
+  body: string;
+  block1: string;
+  block2: string;
+  block3: string;
+  block4: string;
+}
+
+const Child: React.FC<SvgIllustrationProps> = ({ size = 512, color, ...rest }) => {
+  const medium = <ChildMedium color={color} {...rest} />;
+
+  return getIllustration({ size, medium });
+};
+
+export default Child;
