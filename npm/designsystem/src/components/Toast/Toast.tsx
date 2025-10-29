a186861d6f025f2864a4eb012222aa5beb9037c5
Marie Pauline Kristiansen <Marie.Pauline.Kristiansen@nhn.no>
2025-10-29 12:56:37 +0000

    Merged PR 161158: feat(toast): nye komponenter toast og toastlist
    Related work items: #357838

---
+++ npm/designsystem/src/components/Toast/Toast.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Close from '../Close';
+import Icon from '../Icon';
+import CheckFill from '../Icons/CheckFill';
+
+import styles from './styles.module.scss';
+
+export interface ToastProps {
+  /** Sets the data-testid attribute. */
+  testId?: string;
+  /** The title to display in the toast */
+  title: string;
+  /** The message to display in the toast */
+  message?: string;
+  /** Callback when toast is closed */
+  onClose?: () => void;
+}
+
+const Toast: React.FC<ToastProps> = ({ testId, title, message, onClose }) => {
+  const handleClose = (): void => {
+    if (onClose) {
+      onClose();
+    }
+  };
+
+  return (
+    <output className={styles['toast']} data-testid={testId}>
+      <Icon svgIcon={CheckFill} className={styles['toast__icon']} />
+      <div className={styles['toast__text-container']}>
+        <span className={styles['toast__title']}>{title}</span>
+        {message && <span className={styles['toast__description']}>{message}</span>}
+      </div>
+      <Close onClick={handleClose} color="black" className={styles['toast__icon']} testId={`${testId}-close`} />
+    </output>
+  );
+};
+
+export default Toast;
---
+++ npm/designsystem/src/components/ToastList/index.ts
@@ -0,0 +1,3 @@
+import ToastList from './ToastList';
+export * from './ToastList';
+export default ToastList;
