08d165819fa9295ae16c6c77316735cc15172a32
Frederic Hermandsen <Frederic.Hermandsen@nhn.no>
2025-10-29 07:55:29 +0000

    Merged PR 162435: fix: dropdown fokus styling fiks og aschildslot story forbedret
    fix: dropdown fokus styling fiks og aschildslot story forbedret
    
    Related work items: #358846

--- npm/designsystem/src/index.ts
+++ npm/designsystem/src/index.ts
@@ -3,6 +3,7 @@ export { useBreakpoint, Breakpoint } from './hooks/useBreakpoint';
 export { useFocusToggle } from './hooks/useFocusToggle';
 export { useFocusTrap } from './hooks/useFocusTrap';
 export { useHover } from './hooks/useHover';
+export { usePseudoClasses } from './hooks/usePseudoClasses';
 export { useIntersectionObserver } from './hooks/useIntersectionObserver';
 export { useIsVisible } from './hooks/useIsVisible';
 export { useLayoutEvent } from './hooks/useLayoutEvent';
