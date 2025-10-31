e4b5e987394ecf4594d2f3e88b96092987650743
Marie Pauline Kristiansen <Marie.Pauline.Kristiansen@nhn.no>
2025-10-31 08:47:46 +0000

    Merged PR 162912: docs: viser tokens i story
    feat: viser tokens i story
    
    Related work items: #350145

---
+++ npm/designsystem/src/docs/Tokens/TokensExample.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles.scss';
+
+interface TokensExampleProps {
+  colorName?: string;
+  color?: string;
+}
+
+function TokensExample(props: TokensExampleProps): React.JSX.Element {
+  let borderColor = 'transparent';
+  if (props.color === '#ffffff' || props.color === '#fff') {
+    borderColor = '#EAE7E7';
+  }
+  return (
+    <div className="token-container">
+      <div>
+        <div className="token-circle" style={{ backgroundColor: props.color, borderColor: borderColor }}></div>
+      </div>
+      <span className="token-text">{props.colorName}</span>
+    </div>
+  );
+}
+
+export default TokensExample;
