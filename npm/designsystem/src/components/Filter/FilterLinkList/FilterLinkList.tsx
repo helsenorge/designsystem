a2675cc4da3eb4567c6103d3c2717d5edf8bced8
Marie Pauline Kristiansen <marie.pauline.kristiansen@nhn.no>
2026-03-13 10:45:04 +0100

    lag en egen filter linklist

---
+++ npm/designsystem/src/components/Filter/FilterLinkList/FilterLinkList.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+
+import cn from 'classnames';
+
+import { AnalyticsId, IconSize } from '../../../constants';
+import { usePseudoClasses } from '../../../hooks/usePseudoClasses';
+import Icon from '../../Icon';
+import ChevronRight from '../../Icons/ChevronRight';
+import Tag from '../../Tag';
+import TagList from '../../TagList';
+
+import styles from './FilterLinkList.module.scss';
+
+export type LinkType = React.FC<LinkProps>;
+
+export interface CompoundComponent extends React.FC<LinkListProps> {
+  Link: LinkType;
+}
+
+export interface LinkListProps {
+  /** Items in the LinkList */
+  children: React.ReactNode;
+  /** Adds custom classes to the element. */
+  className?: string;
+  /** Sets the data-testid attribute. */
+  testId?: string;
+  /** Ref passed to the ul element */
+  ref?: React.Ref<HTMLUListElement | null>;
+}
+
+type Modify<T, R> = Omit<T, keyof R> & R;
+
+export type LinkProps = Modify<
+  React.HTMLAttributes<HTMLAnchorElement | HTMLButtonElement>,
+  {
+    children?: React.ReactNode;
+    title?: string;
+    chips?: string[];
+    className?: string;
+    /** Ref for lenke/knapp */
+    linkRef?: React.RefObject<HTMLButtonElement | null>;
+    /** Sets the data-testid attribute. */
+    testId?: string;
+    /** Ref passed to the list item element */
+    ref?: React.Ref<HTMLLIElement | null>;
+  }
+>;
+
+export const Link: LinkType = (props: LinkProps) => {
+  const { children, title, chips, className = '', linkRef, testId, ref, ...restProps } = props;
+  const { refObject, isHovered } = usePseudoClasses<HTMLButtonElement | null>(linkRef);
+
+  const liClasses = cn(styles['link-list__item']);
+  const linkClasses = cn(styles['link-list__button'], className);
+
+  return (
+    <li className={liClasses} ref={ref} data-testid={testId} data-analyticsid={AnalyticsId.Link}>
+      <button className={linkClasses} ref={refObject as React.RefObject<HTMLButtonElement>} type="button" {...restProps}>
+        <div className={styles['link-list__button__content']}>
+          {title ? (
+            <>
+              <span>{title}</span>
+              {chips && chips.length > 0 && (
+                <TagList>
+                  {chips.map(chip => (
+                    <Tag key={chip}>{chip}</Tag>
+                  ))}
+                </TagList>
+              )}
+            </>
+          ) : (
+            <>{children}</>
+          )}
+        </div>
+        <Icon svgIcon={ChevronRight} isHovered={isHovered} size={IconSize.XSmall} color={'var(--color-action-graphics-onlight)'} />
+      </button>
+    </li>
+  );
+};
+
+const LinkListComponent: React.FC<LinkListProps> = (props: LinkListProps) => {
+  const { children, className = '', testId, ref } = props;
+
+  const listClassNames = cn(styles['link-list'], className);
+
+  return (
+    <ul ref={ref} className={listClassNames} data-testid={testId} data-analyticsid={AnalyticsId.LinkList}>
+      {React.Children.map(children, (child: React.ReactNode) => {
+        if (React.isValidElement<LinkProps>(child) && child.type === Link) {
+          return React.cloneElement(child);
+        }
+      })}
+    </ul>
+  );
+};
+
+export const FilterLinkList = LinkListComponent as CompoundComponent;
+
+FilterLinkList.displayName = 'FilterLinkList';
+FilterLinkList.Link = Link;
+Link.displayName = 'FilterLinkList.Link';
+
+export default FilterLinkList;
