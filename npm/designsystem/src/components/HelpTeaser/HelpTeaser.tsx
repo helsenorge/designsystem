24bd8f42dde33ab1861d8386abcc7fbce578505c
Frederic Hermandsen <Frederic.Hermandsen@nhn.no>
2026-08-14 06:50:31 +0000

    Merged PR 219504: chore: merge master inn i featurenext
    chore: merge master inn i featurenext
    
    Related work items: #381365

--- npm/designsystem/src/components/HelpTeaser/HelpTeaser.tsx
+++ npm/designsystem/src/components/HelpTeaser/HelpTeaser.tsx
@@ -1,11 +1,24 @@
+import { useId } from 'react';
+
+import classNames from 'classnames';
+
 import type { HNDesignsystemInfoTeaser } from '../../resources/Resources';
-import type { InfoTeaserTags } from '../InfoTeaser';
 import type { TitleTags } from '../Title';
 
+import { useIsMobileBreakpoint } from '@helsenorge/designsystem-react/hooks/useIsMobileBreakpoint';
+
+import { LanguageLocales } from '../../constants';
+import { useExpand } from '../../hooks/useExpand';
+import { useLanguage } from '../../hooks/useLanguage';
+import Icon, { IconSize } from '../Icon';
 import HandWaving from '../Icons/HandWaving';
-import InfoTeaser from '../InfoTeaser';
+import { getResources } from '../InfoTeaser/resourceHelper';
+import Title from '../Title';
 
 import styles from './styles.module.scss';
+
+export type HelpTeaserVariants = 'normal' | 'subdued';
+export type HelpTeaserTags = 'div' | 'section' | 'aside' | 'article';
 
 export interface HelpTeaserProps {
   /** What's in the box? */
@@ -13,7 +26,7 @@ export interface HelpTeaserProps {
   /** Override the default max height for collapsed teaser. Default is 12.25rem */
   collapsedMaxHeight?: string;
   /** Changes the underlying element of the wrapper */
-  htmlMarkup?: InfoTeaserTags;
+  htmlMarkup?: HelpTeaserTags;
   /** Resources for component */
   resources?: Partial<HNDesignsystemInfoTeaser>;
   /** Sets the data-testid attribute */
@@ -22,24 +35,57 @@ export interface HelpTeaserProps {
   title: string;
   /** Markup props for title */
   titleHtmlMarkup?: TitleTags;
+  /** Sets the visual variant */
+  variant?: HelpTeaserVariants;
 }
 
 const HelpTeaser: React.FC<HelpTeaserProps> = props => {
-  const { children, htmlMarkup, resources, testId, title, titleHtmlMarkup, collapsedMaxHeight } = props;
+  const { children, htmlMarkup, resources, testId, title, titleHtmlMarkup, collapsedMaxHeight, variant = 'normal' } = props;
+
+  const [isExpanded, setIsExpanded] = useExpand(false);
+  const { language } = useLanguage<LanguageLocales>(LanguageLocales.NORWEGIAN);
+  const defaultResources = getResources(language);
+  const helpTeaserTextId = useId();
+  const isMobile = useIsMobileBreakpoint();
+
+  const mergedResources: HNDesignsystemInfoTeaser = {
+    ...defaultResources,
+    ...resources,
+  };
+
+  const WrapperTag = htmlMarkup ?? 'div';
+
   return (
-    <InfoTeaser
-      testId={testId}
-      htmlMarkup={htmlMarkup}
-      className={styles.helpteaser}
-      title={title}
-      titleHtmlMarkup={titleHtmlMarkup}
-      resources={resources}
-      svgIcon={HandWaving}
-      buttonClassName={styles['helpteaser__button']}
-      collapsedMaxHeight={collapsedMaxHeight}
-    >
-      {children}
-    </InfoTeaser>
+    <WrapperTag className={styles.wrapper} data-testid={testId}>
+      <div
+        className={classNames(styles.helpteaser, {
+          [styles['helpteaser--collapsed']]: !isExpanded,
+          [styles['helpteaser--subdued']]: variant === 'subdued',
+        })}
+        style={{ maxHeight: !isExpanded ? (collapsedMaxHeight ?? '12.25rem') : undefined }}
+      >
+        <Icon svgIcon={HandWaving} size={isMobile ? IconSize.XSmall : IconSize.Small} className={styles['helpteaser__icon']} />
+        {title && typeof title !== 'undefined' && (
+          <Title testId="titleId" htmlMarkup={titleHtmlMarkup ?? 'h2'} appearance="title6" className={styles['helpteaser__title']}>
+            {title}
+          </Title>
+        )}
+        <div className={styles['helpteaser__text']} aria-hidden={isExpanded ? false : true} id={helpTeaserTextId}>
+          {children}
+        </div>
+      </div>
+      <button
+        type="button"
+        className={styles['helpteaser__button']}
+        onClick={() => {
+          setIsExpanded(!isExpanded);
+        }}
+        aria-expanded={isExpanded}
+        aria-controls={helpTeaserTextId}
+      >
+        {isExpanded ? mergedResources.expandButtonOpen : mergedResources.expandButtonClose}
+      </button>
+    </WrapperTag>
   );
 };
 
